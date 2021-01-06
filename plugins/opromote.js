a7943b662ffa5a455ec228994758c27613dd4753
MFarelS <58540890+MFarelS@users.noreply.github.com>
2021-01-06 13:45:32 +0700

    Create opromote.js

---
+++ plugins/opromote.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, args }) => {
+  let users = m.mentionedJid
+  conn.groupMakeAdmin(m.chat, users)
+}
+handler.command = /^(opromote|oadmin|o\^)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = true
+
+handler.fail = null
+
+module.exports = handler
