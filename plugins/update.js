18e4ce4a80b138c574f078b19c536303ec5a62ec
Nurutomo <tixbig@gmail.com>
2021-01-15 05:12:00 +0700

    fix err

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git pull' + m.fromMe && text ? ' ' + text : '')
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
     conn.reply(m.chat, stdout.toString(), m)
   }
 }
