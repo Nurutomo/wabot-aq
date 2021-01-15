7f0b9563b5d1ebbdca61bf6f8995d5ee0542b83e
Nurutomo <tixbig@gmail.com>
2021-01-15 07:43:08 +0700

    premium feature

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let { JSDOM } = require('jsdom')
 let limit = 30
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
