4babb6a9f9cc4be2c2ab0ac23929b25d65e54825
Nurutomo <tixbig@gmail.com>
2021-01-17 18:26:23 +0700

    dah lah

--- plugins/exec2.js
+++ plugins/exec2.js
@@ -1,5 +1,5 @@
 let { execSync } = require('child_process')
-let handler = async (m, ({ conn, isOwner })) => {
+let handler = async (m, { conn, isOwner }) => {
   if (!isOwner) return
   if (global.conn.user.jid != conn.user.jid) return
   m.reply(execSync(m.text.slice(2)))
