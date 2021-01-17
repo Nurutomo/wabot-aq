a23c67393b489b7ffd63a6c1fcad2b3f4f06b22f
Nurutomo <tixbig@gmail.com>
2021-01-17 18:15:24 +0700

    dah lah

--- plugins/exec2.js
+++ plugins/exec2.js
@@ -2,7 +2,7 @@ let { execSync } = require('child_process')
 let handler = async (m, ({ conn, isOwner })) => {
   if (!isOwner) return
   if (global.conn.user.jid != conn.user.jid) return
-  m.reply(exec(m.text.slice(2)))
+  m.reply(execSync(m.text.slice(2)))
 }
 handler.customPrefix = /^$$ $/
 handler.command = new RegExp
