eb4f1e7c336497cb7bf9b27131bd6ac4bbab138b
Nurutomo <tixbig@gmail.com>
2021-01-18 05:36:53 +0700

    dah lah

--- plugins/sudo.js
+++ plugins/sudo.js
@@ -1,10 +1,12 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text }) => {
   let cm = copy(m)
-  if (m.isGroup) cm.participant = m.mentionedJid[0]
+  let who
+  if (m.isGroup) who = cm.participant = m.mentionedJid[0]
+  else who = m.chat
   cm.key.fromMe = false
   cm.message[m.mtype] = copy(m.msg)
-  txt = text.replace('@' + m.mentionedJid[0].split`@`[0], '').trimStart()
+  txt = text.replace('@' + who.split`@`[0], '').trimStart()
 	if (m.mtype == MessageType.text) cm.message[m.mtype] = txt
 	if (m.mtype == MessageType.extendedText) cm.message[m.mtype].text = txt
   else if (m.mtype == MessageType.image || m.mtype == MessageType.video) cm.message[m.mtype].caption = txt
