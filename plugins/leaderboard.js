86c82307107e5b0538e064c59ca2d48893abd2ca
Nurutomo <tixbig@gmail.com>
2021-01-19 06:21:39 +0700

    new leaderboard

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -1,22 +1,29 @@
 let handler = async (m, { conn, args }) => {
-  let sorted = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
-  let users = sorted.map(v => v[0])
-  let len = args[0] && args[0].length > 0 ? Math.min(1000, Math.max(parseInt(args[0]), 5)) : Math.min(100, sorted.length)
+  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
+  let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
+  let usersExp = sortedExp.map(v => v[0])
+  let usersLim = sortedLim.map(v => v[0])
+  let len = args[0] && args[0].length > 0 ? Math.min(1000, Math.max(parseInt(args[0]), 5)) : Math.min(20, sorted.length)
   let text = `
-• *Leaderboard Top ${len}* •
-Kamu: *${users.indexOf(m.sender) + 1}* dari *${users.length}*
+• *XP Leaderboard Top ${len}* •
+Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
 
-${sorted.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.exp + ' Exp*').join`\n`}
+${sortedExp.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.exp + ' Exp*').join`\n`}
+
+• *Limit Leaderboard Top ${len}* •
+Kamu: *${users.indexOf(m.sender) + 1}* dari *${usersLim.length}*
+
+${sortedLim.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.exp + ' Limit*').join`\n`}
 `.trim()
   conn.reply(m.chat, text, m, {
     contextInfo: {
-      mentionedJid: users.slice(0, len)
+      mentionedJid: [...usersExp.slice(0, len), usersLim.slice(0, len))
     }
   })
 }
-handler.help = ['leaderboard [jumlah user]']
+handler.help = ['leaderboard [jumlah user]', 'lb [jumlah user]]
 handler.tags = ['xp']
-handler.command = /^(leaderboard)$/i
+handler.command = /^(leaderboard|lb)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
