4de3e6f4472f16f1b4af2a05436d71569c040f21
Nurutomo <tixbig@gmail.com>
2021-01-18 21:58:24 +0700

    harga limit dinaikin

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -1,4 +1,4 @@
-const xpperlimit = 35 
+const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
