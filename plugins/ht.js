f806956a3bfcb2c78e8a0027218a41fdf945ae5f
Nurutomo <tixbig@gmail.com>
2021-01-18 21:05:19 +0700

    ok

--- plugins/ht.js
+++ plugins/ht.js
@@ -3,9 +3,12 @@ let handler = async (m, { conn, text }) => {
   if (m.chat in conn.hartatahta) throw 'Masih ada yang sedang membuat\nTeks Harta Tahta\ndi chat ini... tunggu sampai selesai'
   else conn.hartatahta[m.chat] = true
   m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
-  let img = await ht(text ? text : ':v')
-  conn.sendFile(m.chat, img, 'Harta Tahta.png', '*© Nurutomo*\nMade with FFmpeg', m)
-  delete conn.hartatahta[m.chat]
+  try {
+    let img = await ht(text ? text : ':v')
+    conn.sendFile(m.chat, img, 'Harta Tahta.png', '*© Nurutomo*\nMade with FFmpeg', m)
+  } finally {
+    delete conn.hartatahta[m.chat]
+  }
 }
 handler.help = ['tahta <teks>']
 handler.tags = ['tools']
