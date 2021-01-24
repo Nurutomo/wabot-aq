2b42de9cdf08ce7496abc5bdb878b79fee7126ed
Nurutomo <tixbig@gmail.com>
2021-01-24 20:48:53 +0700

    bug

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, participants }) => {
-  if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
     m.reply('Done!')
-  } else m.reply('Ada nomor host disini...')
+  // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
