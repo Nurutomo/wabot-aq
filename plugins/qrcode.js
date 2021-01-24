ef3ef34fe56f5c5f9e1d4168032e0b91c2e625f6
Nurutomo <tixbig@gmail.com>
2021-01-24 20:02:10 +0700

    fitur terlupakan di menu

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -1,8 +1,10 @@
 let qrcode = require("qrcode")
 
 let handler  = async (m, { conn, text }) => {
-  conn.sendFile(m.chat, await qrcode.toDataURL(text, { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+  conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
 }
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
+handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
 handler.owner = false
 handler.mods = false
