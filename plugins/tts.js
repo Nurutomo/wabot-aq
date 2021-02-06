fa3d7abd94d999608c5a8a9af0ab5e006fe9b52f
Nurutomo <tixbig@gmail.com>
2021-02-06 18:35:46 +0700

    fix tts

--- plugins/tts.js
+++ plugins/tts.js
@@ -3,15 +3,27 @@ let fs = require('fs')
 let path = require('path')
 let { spawn } = require('child_process')
 let handler = async (m, { conn, args }) => {
-  if (args.length < 2) return m.reply('Tidak ada teks nya')
-  conn.sendFile(m.chat, await tts(args[0], args.slice(1).join(' ')).catch(err => m.reply(err + '')), 'tts.opus', null, m, true)
+  let lang = 'id'
+  let text = args.slice(1).join(' ')
+  if (args[0].length === 2) lang = args[0]
+  else text = args.join(' ')
+  if (!text) text = lang
+  let res
+  try { res = await tts(text, lang) }
+  catch (e) {
+    m.reply(e + '')
+    res = await tts(text)
+  } finally {
+    conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+  }
 }
 handler.help = ['tts <lang> <teks>']
 handler.tags = ['tools']
 handler.command = /^g?tts$/i
 module.exports = handler
 
-function tts(lang, text) {
+function tts(text, lang = 'id') {
+  console.log(lang, text)
   return new Promise((resolve, reject) => {
     try {
       let tts = gtts(lang)
