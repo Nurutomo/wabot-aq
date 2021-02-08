f55f701af9722c322fd49fb50e0111c8ab9bf603
Nurutomo <tixbig@gmail.com>
2021-02-08 07:03:20 +0700

    :)

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,6 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
