d0995cc153a95e6ca188868f8cc6a94ea81d3743
Nurutomo <tixbig@gmail.com>
2021-02-09 07:14:01 +0700

    theow on no url

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -3,7 +3,7 @@ let { JSDOM } = require('jsdom')
 let limit = 30
 let serverlist = ['id4', 'en60']
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?')
   let server = (args[1] || 'id4').toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], serverlist.includes(server) ? server : 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
