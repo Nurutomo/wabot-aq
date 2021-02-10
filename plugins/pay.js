913670af647a3ac299bf759646631ed6e947165e
Nurutomo <tixbig@gmail.com>
2021-02-10 07:17:44 +0700

    typo

--- plugins/pay.js
+++ plugins/pay.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Masukkan jumlah nxp yang akan diberi'
+  if (!text) throw 'Masukkan jumlah exp yang akan diberi'
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
   if (!who) throw 'Tag salah satu lah'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (isNan) throw 'Hanya angka'
+  if (isNaN(txt)) throw 'Hanya angka'
   let xp = parseInt(txt)
   let exp = xp
   let pjk = Math.ceil(xp * pajak)
