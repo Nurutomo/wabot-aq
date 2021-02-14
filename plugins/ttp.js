4dfb515a854be261791c6776610f94e9677d228c
Nurutomo <tixbig@gmail.com>
2021-02-14 19:51:25 +0700

    fix ttp, add brainly

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
   if (text) {
-    let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text }))
+    let stiker = await sticker2(null, global.API('xteam', '/ttp', { file: '', text }))
     conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
