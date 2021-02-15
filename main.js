970d36c47e7bc12e35b1c4c42906300f5a00159e
roarshop11 <79066325+roarshop11@users.noreply.github.com>
2021-02-15 17:32:46 +0700

    Update

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6281515860089'] // Put your number here
+global.owner = ['62881036804098'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
