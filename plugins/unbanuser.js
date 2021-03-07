a5b880ee3831fed4113fe17fb24f26fd5d1733da
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-03-07 08:29:25 +0700

    Update unbanuser.js
    Typo :v

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text}) => {
 }
 handler.help = ['ban']
 handler.tags = ['owner']
-handler.command = /^ban$/i
+handler.command = /^unban$/i
 handler.rowner = true
 
 module.exports = handler
