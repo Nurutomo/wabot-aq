9cebdf94bd09107f848f6856cb16856271d9c6ef
VendingMp <80427443+VendingMp@users.noreply.github.com>
2021-03-11 04:57:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089'] // Put your number here
+global.owner = ['62895383201495'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
