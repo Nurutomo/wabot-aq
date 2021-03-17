7732c8846614138bb9074fda8e1d047550a6a519
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-17 14:31:45 +0700

    Add Fitur Report/Bug
    Kalo ada masalah pada fitur/ada yang erorr bisa dilaporin keowner lewat fitur ini :)

---
+++ plugins/report.js
@@ -0,0 +1,25 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, 'Silahkan masukkan laporan', m)
+    if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
+    var nomor = m.sender
+    const teks1 = `*「 REPORT 」*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
+    conn.sendMessage('62895337278647@s.whatsapp.net', teks1, MessageType.text)
+    conn.reply(m.chat, '✔️Masalah telah di laporkan ke Owner Bot, laporan palsu/main2 tidak akan ditanggapi!', m)
+}
+handler.help = ['bug <laporan>', 'report <laporan>']
+handler.tags = ['info']
+handler.command = /^(bug|report)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
