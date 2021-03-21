40d893423d4e2edd92beb911f9cd8894d2018b5e
Supun608 <80893855+Supun608@users.noreply.github.com>
2021-03-21 18:19:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6281515860089'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['94754273991'] // Put your number here
+global.mods = [] //  help?
+global.prems = [94754273991] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
