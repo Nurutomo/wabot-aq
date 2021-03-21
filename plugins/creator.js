c7433375faf37a9196994b98a2c4e56f61fa66a6
Supun608 <80893855+Supun608@users.noreply.github.com>
2021-03-21 09:17:42 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+  // this.sendContact(m.chat, '94754273991', 'Nurutomo', m)
+  this.sendContact(m.chat, '0', '94754273991', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
