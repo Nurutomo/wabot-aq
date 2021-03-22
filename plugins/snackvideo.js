da360a7bdbe13eb7ba8b96978d8aeddf16099c47
Nurutomo <tixbig@gmail.com>
2021-03-22 20:24:26 +0700

    undone yet

--- plugins/snackvideo.js
+++ plugins/snackvideo.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
   global.API('xteam', '/dl/', {
     url: args[0]
   }, 'APIKEY')
-  conn.sendFile(m.chat, , '', '', m)
+  conn.sendFile(m.chat, undefined, '', '', m)
 }
 handler.help = [].map(v => v + ' <url>')
 handler.tags = ['downloader']
--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  global.API('xteam', '/dl/', {
+  global.API('xteam', '/dl/twitter', {
     url: args[0]
   }, 'APIKEY')
-  conn.sendFile(m.chat, , '', '', m)
+  conn.sendFile(m.chat, undefined, '', '', m)
 }
 handler.help = [].map(v => v + ' <url>')
 handler.tags = ['downloader']
