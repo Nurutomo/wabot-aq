aca47ee94909e53b156ddd87efee7e3e3f5d328d
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-22 15:10:42 +0700

    Gitu?
    Pasti salah :'v

--- plugins/report.js
+++ plugins/report.js
@@ -5,8 +5,8 @@ let handler = async(m, { conn, text }) => {
     if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks!', m)
     var nomor = m.sender
     const laporan = `*「 REPORT 」*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
- // await conn.reply('6281515860089@s.whatsapp.net', laporan, m)
-    await conn.reply('6281111111111@s.whatsapp.net', laporan, m)
+    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid))
+    m.reply(laporan, jid)
     conn.reply(m.chat, '✔️Masalah telah di laporkan ke Owner Bot, laporan palsu/main2 tidak akan ditanggapi!', m)
 }
 handler.help = ['bug', 'report'].map(v => v + ' <laporan>')
