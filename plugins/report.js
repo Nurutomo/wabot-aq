1a3e016f36e41f6cc4455f82b16af769c66f484d
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-22 07:56:06 +0700

    Update report.js
    Gini aja gimana?

--- plugins/report.js
+++ plugins/report.js
@@ -1,14 +1,15 @@
-const { MessageType } = require('@adiwajshing/baileys')
+// By RC047 :V
 
 let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Silahkan masukkan laporan', m)
-    if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
+    if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks!', m)
     var nomor = m.sender
-    const teks1 = `*「 REPORT 」*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
-    conn.sendMessage('6281515860089@s.whatsapp.net', teks1, MessageType.text)
+    const laporan = `*「 REPORT 」*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
+ // await conn.reply('6281515860089@s.whatsapp.net', laporan, m)
+    await conn.reply('6281111111111@s.whatsapp.net', laporan, m)
     conn.reply(m.chat, '✔️Masalah telah di laporkan ke Owner Bot, laporan palsu/main2 tidak akan ditanggapi!', m)
 }
-handler.help = ['bug <laporan>', 'report <laporan>']
+handler.help = ['bug', 'report'].map(v => v + ' <laporan>')
 handler.tags = ['info']
 handler.command = /^(bug|report)$/i
 handler.owner = false
