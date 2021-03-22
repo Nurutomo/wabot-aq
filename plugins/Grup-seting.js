0412cfd72e927164cb8f7a4f93432cdebb9d5f33
Nurutomo <tixbig@gmail.com>
2021-03-22 20:23:54 +0700

    refactor

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -1,16 +1,18 @@
 let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
-	if(!args || !args[0]) {
-		await conn.updatePresence(m.chat, Presence.composing) 
-		conn.reply(m.chat, `*Format salah! Contoh :*\n\n	*○ ${usedPrefix + command} close*\n	*○ ${usedPrefix + command} open*`, m)
-	} else if(args[0] == 'open') {
-		conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-	} else if(args[0] == 'close') {
-		conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
-	} else {
-		await conn.updatePresence(m.chat, Presence.composing) 
-		conn.reply(m.chat, `*Format salah! Contoh :*\n\n	*○ ${usedPrefix + command} close*\n	*○ ${usedPrefix + command} open*`, m)
-	} 
+	let isClose = { // Switch Case Like :v
+		'open': false,
+		'close': true,
+	}[(args[0] || '')]
+	await conn.updatePresence(m.chat, Presence.composing)
+	if (isClose === undefined)
+		throw `
+*Format salah! Contoh :*
+
+  *○ ${usedPrefix + command} close*
+  *○ ${usedPrefix + command} open*
+`.trim()
+	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
 }
 handler.help = ['group *open / close*']
 handler.tags = ['group']
