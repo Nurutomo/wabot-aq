2c6428c86f2d60423e0e31a88e5f94900e3209ce
Nurutomo <tixbig@gmail.com>
2021-03-25 07:22:51 +0700

    antilink ?

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,12 +1,18 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = m => {
+handler.before = function (m, { user }) {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) m.reply('Hapus!!')
+  if (chat.antiLink && isGroupLink) {
+    m.reply('Hapus!!')
+    if (global.opts['restrict']) {
+      // if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
   return true
 }
 
