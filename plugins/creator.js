124f927ee97329d28dd87c79b490c2bbc2c320e5
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-03-25 10:33:26 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+  this.sendContact(m.chat, '6288235435804', 'DrawlNag Contact', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
