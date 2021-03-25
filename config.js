87e6bf89595cb0f7c723931bcc86c889f850f9cc
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-03-25 10:26:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089'] // Put your number here
+global.owner = ['6281357302007','6288235435804'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -8,12 +8,12 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://api.xteam.xyz': 'Masukkan_Apikey'
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'I hope you\'Subscribe'
+global.author = 'Drawl Nag'
 
 global.multiplier = 69 // The higher, The harder levelup
 
