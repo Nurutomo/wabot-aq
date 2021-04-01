4eb5942986a3a90477f72db18c34b346739334df
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-04-01 18:06:39 +0700

    Expired ketika mematikan BOT.

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -8,7 +8,7 @@ let who
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].prems = true
   var nomor = m.sender
-    m.reply(`*Done berhasil added User✅*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
+    m.reply(`*Done berhasil added User✅*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* Until the bot dies\n*Thanks For Added Premium !*`)
   // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['addprems <nomor>']
