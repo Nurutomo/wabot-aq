fbef2d5b231abfa4ea0801cbe3b1fa0d06fb9340
MFarelS <58540890+MFarelS@users.noreply.github.com>
2021-04-19 09:29:50 +0700

    fix

--- plugins/nulis2.js
+++ plugins/nulis2.js
@@ -62,7 +62,7 @@ let handler  = async (m, { conn, args }) => {
 }
 handler.help = ['n'].map(v => v + 'ulis2 <teks>')
 handler.tags = ['tools']
-handler.command = /^nulis$2/i
+handler.command = /^nulis2$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
