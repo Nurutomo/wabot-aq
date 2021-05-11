00fd79996fcf8a5d9dc97055c5a5cc79a409772b
Nurutomo <tixbig@gmail.com>
2021-05-11 22:45:31 +0700

    simulate welcome & fix setwelcome

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     if (isROwner) global.conn.bye = text
     else if (isOwner) conn.bye = text
-    else global.DATABASE._data.chats[m.chat].sBye = text
+    global.DATABASE._data.chats[m.chat].sBye = text
     m.reply('Bye berhasil diatur\n@user (Mention)')
   } else throw 'Teksnya mana?'
 }
