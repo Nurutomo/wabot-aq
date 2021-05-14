99df12aa90f064704e756a7d0f72fb5a66d67da7
anshuljai <anshuljaisalmeria5@gmail.com>
2021-05-14 09:33:59 +0530

    Create stickmaker.js
    😁

---
+++ stickmaker.js
@@ -0,0 +1,33 @@
+const uploadImage = require('../lib/uploadImage')
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+//made by anshul
+let handler = async (m, { conn, args, usedPrefix }) => {
+    if (args.length == 0) return conn.reply(m.chat, `To use *${usedPrefix}stickmaker*\nPlease type: *${usedPrefix}stickmaker* [Query]\nExample: *${usedPrefix}stickmaker jail*\n\n*List Query:*\n_> gay_\n_> glass_\n_> wasted_\n_> jail_`, m)
+    if (args[0] == 'jail' || args[0] == 'gay' || args[0] == 'glass' || args[0] == 'wasted')
+  await m.reply(global.wait)
+try {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let stick = `https://some-random-api.ml/canvas/` + args[0] + `?avatar=${url}`
+  let stiker = await sticker(null, stick, 'Dark', 'Bot')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+} catch (e) {
+  m.reply('Conversion Failed')
+  }
+}
+
+handler.help = ['stickmaker (caption|reply media)']
+handler.tags = ['sticker']
+handler.command = /^(stickmaker)$/i
+handler.limit = true
+handler.group = false
+handler.register = true
+//made by Anshul
+module.exports = handler
