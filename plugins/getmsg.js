5d81b15b67db0a9ac82099535208a3505d3396b2
Nurutomo <tixbig@gmail.com>
2021-05-17 18:43:33 +0700

    fix add & getmsg

--- plugins/getmsg.js
+++ plugins/getmsg.js
@@ -1,9 +1,21 @@
 let handler = async (m, { conn, command, usedPrefix, text }) => {
     let which = command.replace(/get/i, '')
     if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat list nya`
-    let msgs = global.DATABASE._data.msgs
+    let msgs = global.DATABASE.data.msgs
     if (!text in msgs) throw `'${text}' tidak terdaftar di list pesan`
-    let _m = await conn.serializeM(msgs[text])
+    let _m = conn.serializeM(JSON.parse(JSON.stringify(msgs[text]), (_, v) => {
+        if (
+            v !== null &&
+            typeof v === 'object' &&
+            'type' in v &&
+            v.type === 'Buffer' &&
+            'data' in v &&
+            Array.isArray(v.data)) {
+            return Buffer.from(v.data)
+        }
+        return v
+    }))
+    // m.reply(`[debug]\n${require('util').format(_m)}`)
     await _m.copyNForward(m.chat, true)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'get' + v + ' <text>')
