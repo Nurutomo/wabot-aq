92aa16101104a4542859ae3857c768ad51e3f35d
Nurutomo <tixbig@gmail.com>
2021-05-21 13:59:24 +0700

    dont log error

--- handler.js
+++ handler.js
@@ -14,8 +14,9 @@ module.exports = {
       m.exp = 0
       m.limit = false
       try {
-        let user
-        if (user = global.DATABASE._data.users[m.sender]) {
+        let user = global.DATABASE._data.users[m.sender]
+        if (typeof user !== 'object') global.DATABASE._data.users[m.sender] = {}
+        if (user) {
           if (!isNumber(user.exp)) user.exp = 0
           if (!isNumber(user.limit)) user.limit = 10
           if (!isNumber(user.lastclaim)) user.lastclaim = 0
@@ -45,8 +46,9 @@ module.exports = {
           autolevelup: false,
         }
 
-        let chat
-        if (chat = global.DATABASE._data.chats[m.chat]) {
+        let chat = global.DATABASE._data.chats[m.chat]
+        if (typeof chat !== 'object') global.DATABASE._data.chats[m.chat] = {}
+        if (chat) {
           if (!'isBanned' in chat) chat.isBanned = false
           if (!'welcome' in chat) chat.welcome = false
           if (!'detect' in chat) chat.detect = false
@@ -68,7 +70,7 @@ module.exports = {
           antiLink: false,
         }
       } catch (e) {
-        console.log(e, global.DATABASE.data)
+        console.error(e)
       }
       if (opts['nyimak']) return
       if (!m.fromMe && opts['self']) return
