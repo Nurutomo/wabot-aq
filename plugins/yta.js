72e37b49d850e6137fcebbe327f74cfeec0475b1
Nurutomo <tixbig@gmail.com>
2021-05-21 12:49:40 +0700

    change y2mate server

--- plugins/yta.js
+++ plugins/yta.js
@@ -3,7 +3,7 @@ const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let server = (args[1] || 'id4').toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
+  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
