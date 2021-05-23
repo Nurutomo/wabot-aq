a1f1ec26f5deaf5191d5e054333af343c8780f4e
Nurutomo <tixbig@gmail.com>
2021-05-23 16:30:24 +0700

    jadiin admin :v

--- plugins/accbang.js
+++ plugins/accbang.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw 'Padahal udah jadi admin'
   await conn.groupMakeAdmin(m.chat, [m.sender])
 }
-handler.command = /^admin dong$/i
+handler.command = /^admin.$/i
 handler.rowner = true
 handler.botAdmin = true
 module.exports = handler
