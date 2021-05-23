8c5ef0e4eafcb72b2bbd6f83540bf8f6668f384b
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-23 23:04:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nWhatsApp Bot', {
+CFonts.say('NK\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
