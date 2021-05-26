827ce1c7da7cb831bec8d0f0e4119d0a3b9edaca
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-26 18:28:37 +0530

    Update tebaklagu_ans.js

--- plugins/tebaklagu_ans.js
+++ plugins/tebaklagu_ans.js
@@ -3,17 +3,17 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/TEBAK JUDUL LAGU/i.test(m.quoted.text)) return !0
     conn.tebaklagu = conn.tebaklagu ? conn.tebaklagu : {}
-    if (!(id in conn.tebaklagu)) return m.reply('Soal itu telah berakhir')
+    if (!(id in conn.tebaklagu)) return m.reply('That's right. That question has ended')
     if (m.quoted.id == conn.tebaklagu[id][0].id) {
         let json = JSON.parse(JSON.stringify(conn.tebaklagu[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.judul.toLowerCase()) {
             global.DATABASE._data.users[m.sender].exp += conn.tebaklagu[id][2]
-            m.reply(`*Benar!*\n+${conn.tebaklagu[id][2]} XP`)
+            m.reply(`*Right!*\n+${conn.tebaklagu[id][2]} XP`)
             clearTimeout(conn.tebaklagu[id][3])
             delete conn.tebaklagu[id]
-        } else if (m.text.toLowerCase().endsWith(json.judul.split` `[1])) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        } else if (m.text.toLowerCase().endsWith(json.judul.split` `[1])) m.reply(`*A little more!*`)
+        else m.reply(`*Wrong!*`)
     }
     return !0
 }
