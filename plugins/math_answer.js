ebec8088376a8e826f377604924e198bb2ae3875
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-27 18:29:31 +0530

    Update math_answer.js

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -4,20 +4,20 @@ handler.before = async function (m) {
   let id = m.chat
   if (!m.quoted || m.quoted.sender != this.user.jid || !/^Berapa hasil dari/i.test(m.quoted.text)) return !0
   this.math = this.math ? this.math : {}
-  if (!(id in this.math)) return m.reply('Soal itu telah berakhir')
+  if (!(id in this.math)) return m.reply('That question has ended')
   if (m.quoted.id == this.math[id][0].id) {
     let math = JSON.parse(JSON.stringify(this.math[id][1]))
     if (m.text == math.result) {
       global.DATABASE._data.users[m.sender].exp += math.bonus
       clearTimeout(this.math[id][3])
       delete this.math[id]
-      m.reply(`*Jawaban Benar!*\n+${math.bonus} XP`)
+      m.reply(`*Correct answer!*\n+${math.bonus} XP`)
     } else {
       if (--this.math[id][2] == 0) {
         clearTimeout(this.math[id][3])
         delete this.math[id]
-        m.reply(`*Kesempatan habis!*\nJawaban: *${math.result}*`)
-      } else m.reply(`*Jawaban Salah!*\nMasih ada ${this.math[id][2]} kesempatan`)
+        m.reply(`*Chance ran out!*\nAnswer: *${math.result}*`)
+      } else m.reply(`*Wrong answer!*\nThere are still ${this.math[id][2]} chances`)
     }
   }
   return !0
