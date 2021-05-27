37eaa7d5ee8664c1bf2b4cff5e919074f48e66ad
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-27 17:38:24 +0530

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
   if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
-  else throw 'Uhm...Teksnya?'
+  else throw 'Uhm...Text?'
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
