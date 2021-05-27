f26a699ba0690f2cffd1d88e00cefcff5a1c1971
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-27 11:10:19 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah'
+  if (!who) throw 'Tag one'
   txt = text.replace('@' + who.split`@`[0], '').trimStart()
   return conn.sendContact(m.chat, who, txt || conn.getName(who), m)
 }
