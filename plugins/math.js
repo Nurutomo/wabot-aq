d2c6046de4bd3da66201a591046989b588b2eb85
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-27 18:13:57 +0530

    Update math.js

--- plugins/math.js
+++ plugins/math.js
@@ -12,13 +12,13 @@ Mode: ${Object.keys(modes).join(' | ')}
 Contoh penggunaan: ${usedPrefix}math medium
 `.trim()
   let id = m.chat
-  if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
+  if (id in conn.math) return conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.math[id][0])
   let math = genMath(mode)
   conn.math[id] = [
-    await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
+    await conn.reply(m.chat, `How many results from *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} second\nBonus Correct answer: ${math.bonus} XP`, m),
     math, 4,
     setTimeout(() => {
-      if (conn.math[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, conn.math[id][0])
+      if (conn.math[id]) conn.reply(m.chat, `Time out!\nThe answer is\\ ${math.result}`, conn.math[id][0])
       delete conn.math[id]
     }, math.time)
   ]
