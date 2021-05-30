0f1d780e610436d72d27d9ed51f12962e822f389
narayan <48875039+NinjateckNK@users.noreply.github.com>
2021-05-31 00:44:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+sudo apt-get update && upgrade
 sudo apt-get install nodejs -y
+apt install curl
+curl -qL https://www.npmjs.com/install.sh | sh
 sudo apt-get install ffmpeg -y
 sudo apt-get install imagemagick -y
 npm install
