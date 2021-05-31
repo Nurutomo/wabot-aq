d91a4df1744edfe782f13b74901f72624508fb7a
Nurutomo <tixbig@gmail.com>
2021-05-31 12:34:49 +0700

    update to this github repo :v

--- plugins/update.js
+++ plugins/update.js
@@ -1,9 +1,9 @@
 let { execSync } = require('child_process')
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-    conn.reply(m.chat, stdout.toString(), m)
+    let stdout = execSync('git remote set-url origin https://github.com/Nurutomo/wabot-aq.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
   }
 }
 handler.help = ['update']
