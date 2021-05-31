8a163daa59a20cc570780f9849527c197f78337d
ariffb <76026450+ariffb25@users.noreply.github.com>
2021-05-31 16:48:47 +0700

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -1,15 +1,13 @@
 let handler = async (m, { conn, command, args }) => {
-  let chats = args.length > 0 && /group|gc/i.test(args[0]) ? conn.chats.array.filter(v => v.jid.endsWith('g.us') && !v.pin).map(v => v.jid) : [m.chat]
-  let chatsPrivate = args.length > 0 && /chat|private/i.test(args[0]) ? conn.chats.array.filter(v => v.jid.endsWith('.net') && !v.pin).map(v => v.jid) : [m.chat]
+  let chats
+  if (/group|gc/i.test(args[0])) chats = conn.chats.array.filter(v => v.jid.endsWith('g.us') && !v.pin).map(v => v.jid)
+  else if (/chat|private/i.test(args[0])) chats = conn.chats.array.filter(v => v.jid.endsWith('.net') && !v.pin).map(v => v.jid)
+  else chats = [m.chat]
   let isDelete = /^(clear|delete)/i.test(command)
   for (let id of chats) {
     if (isDelete) await conn.modifyChat(id, 'delete', {
       includeStarred: false
     }).catch(console.log)
-    await conn.modifyChat(id, 'mute', -Math.floor(new Date / 1e3) * 1e3 - 1e3).catch(console.log)
-  }
-  for (let id of chatsPrivate) {
-    if (isDelete) await conn.modifyChat(id, 'delete').catch(console.log)
     await conn.modifyChat(id, 'mute', -Math.floor(new Date / 1e3) * 1e3 - 1e3).catch(console.log)
   }
   conn.reply(m.chat, chats.length + ' chat ' + args[0] + ' telah dib' + (isDelete ? 'ersihkan' : 'isukan selamanya'), m)
