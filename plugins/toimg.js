c078e66c9d87b50ea24fd7e2521eb311eb1a7101
Nurutomo <tixbig@gmail.com>
2021-05-31 12:35:52 +0700

    magick convert

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -2,15 +2,15 @@ const { spawn } = require('child_process')
 const util = require('util')
 const { MessageType } = require('@adiwajshing/baileys')
 
-let handler  = async (m, { conn }) => {
+let handler = async (m, { conn }) => {
   if (!m.quoted) return conn.reply(m.chat, 'Tag stikernya!', m)
-  let q = { message: { [m.quoted.mtype]: m.quoted }}
+  let q = { message: { [m.quoted.mtype]: m.quoted } }
   if (/sticker/.test(m.quoted.mtype)) {
     let sticker = await conn.downloadM(q)
     if (!sticker) throw sticker
     let bufs = []
-    let im = spawn('convert', ['webp:-', 'png:-'])
-    im.on('error',e =>  conn.reply(m.chat, util.format(e), m))
+    let im = spawn('magick', ['convert', 'webp:-', 'png:-'])
+    im.on('error', e => conn.reply(m.chat, util.format(e), m))
     im.stdout.on('data', chunk => bufs.push(chunk))
     im.stdin.write(sticker)
     im.stdin.end()
