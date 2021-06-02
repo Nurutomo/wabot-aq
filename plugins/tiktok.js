9d2de7e5f78c9382d9da59a8f46fa758d5c9a406
Nurutomo <tixbig@gmail.com>
2021-06-02 16:25:48 +0700

    tiktok fix maybe?

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -7,13 +7,24 @@ let handler = async (m, { conn, args }) => {
   if (res.status !== 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  await conn.sendFile(m.chat, json.info[0].videoUrl, 'tiktok.mp4', `
-${json.info [0].text}
+  let url = json.server_1 || json.info[0].videoUrl || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = json.info[0].text
+  for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
+  await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+▶ ${json.info[0].playCount} Views
+❤ ${json.info[0].diggCount} Likes
+🔁 ${json.info[0].shareCount} Shares
+💬 ${json.info[0].commentCount} Comments
+🎵 ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
+- *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
+- *Desc:*
+${txt}
   `.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^(tiktok(dl)?)$/i
+handler.command = /^(tik(tok)?(dl)?)$/i
 
 module.exports = handler
