4f462ee0229a76a9a29bce60a564e96243435040
Nurutomo <tixbig@gmail.com>
2021-06-02 14:54:55 +0700

    Update X - Team

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -1,14 +1,14 @@
 // Terimakasih kpd RC047 :v
 // Fitur By Xteams
+// Modified by Nurutomo (Update Xteam :v)
 
 const { sticker } = require('../lib/sticker')
-const { MessageType } = require('@adiwajshing/baileys')
-
+const defaultType = 'whatsapp'
 let handler = async (m, { usedPrefix, conn, args, text }) => {
   let [tipe, emoji] = text.includes('|') ? text.split('|') : args
   if (tipe && !emoji) {
     emoji = tipe
-    tipe = 'whatsapp'
+    tipe = defaultType
   }
   if (!emoji) throw `Silahkan masukan emojinya\n\nMisal ${usedPrefix}semoji whatsapp 😎\n\nList Tipe:
 - whatsapp
@@ -16,11 +16,19 @@ let handler = async (m, { usedPrefix, conn, args, text }) => {
 - apple
 - google
 - microsoft`
-  let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopng' + encodeURI(tipe.trim().toLowerCase()), { emo: emoji.trim() }, 'APIKEY'), global.packname, global.author)
-//   m.reply(`
-// Tipe: ${tipe.trim().toLowerCase()}
-// Emoji: ${emoji.trim()}
-// `.trim())
+  emoji = emoji.trim()
+  tipe = tipe.trim().toLowerCase()
+
+  let res = await fetch(global.API('xteam', '/sticker/emojitopng', { emo: emoji.trim() }, 'APIKEY'))
+  if (!res.ok) throw res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  if (!(tipe in json.result)) tipe = defaultType
+  let stiker = await sticker(null, json.result[tipe], global.packname, global.author, emoji)
+  //   m.reply(`
+  // Tipe: ${tipe}
+  // Emoji: ${emoji}
+  // `.trim())
   m.reply(stiker)
 }
 handler.help = ['semoji [tipe] <emoji>']
