69f825b1c7a7c6354860b889f7553d4f72b0ccfe
Nurutomo <tixbig@gmail.com>
2021-06-03 16:21:10 +0700

    use old method

--- plugins/opromote.js
+++ plugins/opromote.js
@@ -1,19 +1,17 @@
-let handler = async (m, { conn, args }) => {
-  let users = m.mentionedJid
-  conn.groupMakeAdmin(m.chat, users)
+let handler = async (m, { conn, participants }) => {
+  let members = participants.filter(member => !member.isAdmin).map(member => member.jid)
+  let users = m.mentionedJid.filter(user => members.includes(user))
+  for (let user of users) await conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
 }
 handler.help = ['promote','admin','^'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
+
 handler.command = /^(opromote|oadmin|o\^)$/i
+
 handler.owner = true
-handler.mods = false
-handler.premium = false
 handler.group = true
-handler.private = false
 
 handler.admin = false
 handler.botAdmin = true
-
-handler.fail = null
 
 module.exports = handler
