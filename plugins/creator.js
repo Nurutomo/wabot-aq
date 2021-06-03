007fb8c52cf1e456edb926ef928bc55c0c6372ae
Anym <68510045+AnymPedia@users.noreply.github.com>
2021-06-04 01:05:27 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  this.sendContact(m.chat, '6285733476334', 'Anym', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
