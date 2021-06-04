b7474fc164fe62e10411723d6ec220725db66647
Nurutomo <tixbig@gmail.com>
2021-06-04 16:42:54 +0700

    invert: false?

--- plugins/img2braille.js
+++ plugins/img2braille.js
@@ -3,13 +3,13 @@ let { join } = require('path')
 const tmp = join(__dirname, '../tmp')
 async function handler(m) {
     let { braillefy } = require('img2braille')
-    
+
     let q = m.quoted ? m.quoted : m
     if (!/^image/.test(q.mimetype)) throw 'Not an Image!'
     let filename = join(tmp, + new Date + '.png')
     await fs.writeFile(filename, await q.download())
     m.reply(await braillefy(filename, 30, {
-        invert: true,
+        invert: false,
         dither: true
     }))
     await fs.unlink(filename)
