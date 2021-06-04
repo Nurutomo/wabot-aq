df4d9c6b34d91846aa15a60de2f5f7afefb94181
Nurutomo <tixbig@gmail.com>
2021-06-04 16:35:34 +0700

    list jadibot

---
+++ plugins/listjadibot.js
@@ -0,0 +1,8 @@
+async function handler(m, { usedPrefix }) {
+  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
+  m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+}
+handler.command = handler.help = ['listjadibot']
+handler.tags = ['jadibot']
+
+module.exports = handler
