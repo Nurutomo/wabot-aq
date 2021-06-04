77e56b9594d5a5ed7603dad8e3a342763c01381a
Nurutomo <tixbig@gmail.com>
2021-06-04 16:35:54 +0700

    more custom daily claim

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,9 +1,9 @@
-let handler = async (m, { conn }) => {
-  if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    global.DATABASE._data.users[m.sender].exp += 500
-    m.reply('+500 XP')
-    global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
-  } else throw 'Anda sudah mengklaim klaim harian hari ini'
+const free = 500
+let handler = async (m) => {
+  if (new Date - global.DATABASE._data.users[m.sender].lastclaim < 86400000) throw 'Anda sudah mengklaim klaim harian hari ini'
+  global.DATABASE._data.users[m.sender].exp += free
+  m.reply(`+${free} XP`)
+  global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
 }
 handler.help = ['daily', 'claim']
 handler.tags = ['xp']
