e6e46232add8f9058e69aee6e919ef5b5b2b3674
Nurutomo <tixbig@gmail.com>
2021-06-04 16:33:42 +0700

    instant levelup

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -9,11 +9,13 @@ Level *${user.level} (${user.exp - min}/${xp})*
 Kurang *${max - user.exp}* lagi!
 `.trim()
   }
-  user.level++
-  m.reply(`
+  let before = user.level * 1
+	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+	if (before !== user.level) m.reply(`
 Selamat, anda telah naik level!
-*${user.level - 1}* -> *${user.level}*
-  `.trim())
+*${before}* -> *${user.level}*
+gunakan *.profile* untuk mengecek
+	`.trim())
 }
 
 handler.help = ['levelup']
