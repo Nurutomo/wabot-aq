bf2a7010e790f7782d2049be3312a37efc195075
Nurutomo <tixbig@gmail.com>
2021-06-05 17:01:38 +0700

    Invalid Date

--- plugins/zodiac.js
+++ plugins/zodiac.js
@@ -2,6 +2,7 @@ let handler = (m, { usedPrefix, command, text }) => {
     if (!text) throw `contoh:\n${usedPrefix + command} 2002 02 25`
 
     const date = new Date(text)
+    if (date == 'Invalid Date') throw date
     const d = new Date()
     const [tahun, bulan, tanggal] = [d.getFullYear(), d.getMonth() + 1, d.getDate()]
     const birth = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
