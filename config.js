b8682804dee3c1b28a047cf7b4a33e423ee39291
NewPabloA <85406737+NewPabloA1@users.noreply.github.com>
2021-06-05 12:33:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.owner = ['6281515860089','5565996511068'] //
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'NewPabloA'
+global.author = '65996511068'
 
 global.multiplier = 69 // The higher, The harder levelup
 
