3bdf2d8ed8d8f7a483e255ce6a01e13db6df62e5
Nurutomo <tixbig@gmail.com>
2021-06-06 06:06:40 +0700

    asynchronous

--- plugins/citacita.js
+++ plugins/citacita.js
@@ -1,17 +1,17 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
-fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
-.then(res => res.text())
-.then(body => {
-let json = body.split('\n')
-let cita = json[Math.floor(Math.random() * json.length)]
+let res = await fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
+let txt = await res.text()
+
+let json = txt.split('\n')
+let cita = arr[Math.floor(Math.random() * arr.length)]
 conn.sendFile(m.chat, cita, 'cita.mp3', null, m, true, {
   type: 'audioMessage', // paksa tanpa convert di ffmpeg
   ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
-})}
-handler.help = [''].map(v => v + ' <>')
-handler.tags = ['']
+}
+
+
 handler.customPrefix = /^cita cita|citacita|cita$/i
 handler.command = new RegExp
 handler.owner = false
