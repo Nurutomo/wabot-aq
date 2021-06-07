9f11c7ec1544c8678a3cd016114153e91d21187c
Nurutomo <tixbig@gmail.com>
2021-06-07 18:11:56 +0700

    Simplify :v

--- plugins/bannedList.js
+++ plugins/bannedList.js
@@ -1,34 +1,20 @@
 let handler = async (m, { conn, usedPrefix }) => {
-    let chats = global.DATABASE.data.chats
-    let users = global.DATABASE.data.users
-
-    var text = ""
-    var i = 1
-    for (let jid in chats) {
-        if (chats[jid].isBanned) {
-            text += `│ ${i}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}\n│ ${jid}\n`
-            i += 1
-        }
-    }
-
-    var pes = ""
-    var n = 1
-    for (let jid in users) {
-        if (users[jid].banned) {
-            pes += `│ ${n}. ${conn.getName(jid, true) == undefined ? 'Unknown' : conn.getName(jid, true)}`
-            n += 1
-        }
-    }
-    return conn.reply(m.chat, `┌ *Daftar Chat Terbanned*
-│ Total : ${i - 1} Chat
-│ \n${text}
+    let chats = Object.entries(global.DATABASE.data.chats).filter(chat => chat[1].isBanned)
+    let users = Object.entries(global.DATABASE.data.users).filter(user => user[1].banned)
+    
+    m.reply(`
+┌ *Daftar Chat Terbanned*
+│ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
+│ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+│ ${jid}
+`.trim()).join('\n') : ''}
 └────
 
 ┌ *Daftar User Terbanned*
-│ Total : ${n - 1} User
-│ \n${pes}
-└────
-`, m)
+│ Total : ${users.length} User${users ? '\n' + users.map(([jid], i) => `
+│ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+│ ${jid}
+`.trim()).join('\n') : ''}
 }
 handler.help = ['bannedlist']
 handler.tags = ['info']
