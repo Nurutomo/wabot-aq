e79525b1f16a14dab1acad95911dcb4db5db6950
Nurutomo <tixbig@gmail.com>
2021-06-08 19:19:59 +0700

    encodeURI is important!!

--- plugins/memeg.js
+++ plugins/memeg.js
@@ -2,16 +2,20 @@ const uploadFile = require('../lib/uploadFile')
 const uploadImage = require('../lib/uploadImage')
 let handler = async (m, { conn, text }) => {
   let [t1, t2] = text.split `|`
-  if (!t1) throw 'apa'
-  if (!t2) throw 'apa'
+  if (!t1) throw 'No Text'
+  if (!t2) {
+    t2 = t1
+    t1 = ''
+  }
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Apa`
+  if (!mime) throw `Unknown Mimetype`
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(img)
-  conn.sendFile(m.chat, `https://api.memegen.link/images/custom/${t1}/${t2}.png?background=${link}`, 'Katanya Game Ini Sangat Menakutkan & Sulit! NAMATIN Granny 3.png', `Udahan yu`, m)
+  let link = await uploadImage(img).catch(e => uploadFile(img))
+  conn.sendFile(m.chat, global.API('https://api.memegen.link', `/images/custom/${encodeURIComponent(t1)}/${encodeURIComponent(t2)}.png`, {
+    background: link
+  }), 'meme.png', `Nih :|`, m)
 }
 handler.help = ['memeg'].map(v => v + '<apa|apa>')
 handler.tags = ['tools']
