673f47843b6b3415513a7d8555fd6390be9a90d7
Nurutomo <tixbig@gmail.com>
2021-06-11 09:38:57 +0700

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -2,15 +2,19 @@
 
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  if (!text) throw 'missing title query'
+  if (!text) throw 'Missing \'title\' query'
   let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
     title: text
-  }
-  ))
+  }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.error) conn.reply(m.chat, `*${json.title}*\n_${json.author}_\n\n${json.lyrics}`, m)
-  else throw json.eror
+  if (json.error) throw json.error
+  m.reply(`
+*${json.title}*
+_${json.author}_
+
+${json.lyrics}
+`.trim())
 }
 handler.help = ['lirik'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
