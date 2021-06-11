833eae7f620c73c35a11b75a8e861b1a28d5e2ae
Nurutomo <tixbig@gmail.com>
2021-06-11 09:36:13 +0700

    no profile picture

--- plugins/gay.js
+++ plugins/gay.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
-    avatar: await conn.getProfilePicture(who).catch(_ => ''),
+    avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'gay.png', 'siapa disini yang cita citanya menjadi gay dan memperkosa gw gays?', m)
 }
 
