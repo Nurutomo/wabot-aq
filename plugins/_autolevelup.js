9b6d00564111c4c9cdbdeddbd0a16967acab286b
Nurutomo <tixbig@gmail.com>
2021-06-13 21:28:07 +0700

    indent

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -2,7 +2,7 @@ let handler = m => m
 
 let levelling = require('../lib/levelling')
 handler.before = m => {
-  let user = global.DATABASE.data.users[m.sender]
+	let user = global.DATABASE.data.users[m.sender]
 	if (!user.autolevelup) return !0
 	let before = user.level * 1
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
