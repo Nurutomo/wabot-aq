722ac7ccf1bd86b9667df93e9739d0c19175e30e
Nurutomo <tixbig@gmail.com>
2021-06-14 09:33:46 +0700

    value.text

--- plugins/listCmdWithMedia.js
+++ plugins/listCmdWithMedia.js
@@ -1,9 +1,13 @@
 module.exports = Object.assign(m => m.reply(`
 *LIST HASH*
 \`\`\`
-${Object.entries(global.DATABASE.data.sticker).map(([key, value], index) => `${index + 1}. ${key}: ${value}`).join('\n')}
+${Object.entries(global.DATABASE.data.sticker).map(([key, value], index) => `${index + 1}. ${key}: ${value.text}`).join('\n')}
 \`\`\`
-`.trim()), {
+`.trim(), null, {
+    contextInfo: {
+        mentionedJid: Object.values(global.DATABASE.data.sticker).map(x => x.mentionedJid).reduce((a,b) => [...a, ...b], [])
+    }
+}), {
     help: ['cmd'].map(v => 'list' + v + ' <text>'),
     tags: ['database'],
     command: ['listcmd']
