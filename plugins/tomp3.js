72a62b8b4f5381b3d94b6e2b58ec21354a86e208
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-06-16 09:56:37 +0800

    hannah.mp3

---
+++ plugins/tomp3.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
+        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+        if (/video|audio/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(q)
+            let ran = getRandom('.mp3')
+            exec(`ffmpeg -i ${media} ${ran}`, (err, stderr, stdout) => {
+                fs.unlinkSync(media)
+                if (err) throw `_*Error!*_`
+                let buff = fs.readFileSync(ran)
+                conn.sendFile(m.chat, buff, ran, null, m, null)
+                fs.unlinkSync(ran)
+            })
+        } else throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
+    } catch (e) {
+        throw e
+    }
+}
+handler.help = ['tomp3']
+handler.tags = ['audio']
+handler.command = /^(tomp3)$/i
+module.exports = handler
+
+
+const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`
+}
