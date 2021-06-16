631b3e8b1098f9bbc6b337c4c3a42d6ea26b1a72
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-06-17 06:53:09 +0800

    gitu?

--- plugins/memek.js
+++ plugins/memek.js
@@ -1,7 +1,9 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
   let res = await fetch(global.API('https://some-random-api.ml', '/meme'))
+  if (!res.ok) throw await res.text()
   let json = await res.json()
+  if (!json.image) throw 'Err!'
   conn.sendFile(m.chat, json.image, 'memek.png', json.caption, m)
 }
 
