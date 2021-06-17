aba493bad1c984c62c4a2bf90753da2fadc94312
Nurutomo <tixbig@gmail.com>
2021-06-17 20:36:26 +0700

    Update and rename memek.js to meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.image) throw 'Err!'
-  conn.sendFile(m.chat, json.image, 'memek.png', json.caption, m)
+  conn.sendFile(m.chat, json.image, 'meme.png', json.caption, m)
 }
 
 handler.help = ['meme']
