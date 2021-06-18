e8d6702f07846c851bf103eca4a49a97048683ff
Nurutomo <tixbig@gmail.com>
2021-06-18 17:40:35 +0700

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -1,24 +1,26 @@
-let handler  = async (m, { conn, args, text }) => {
-     if (!text) throw 'Cari apa ?'
- if (text) m.reply('Searching.....')
-let gis = require('g-i-s')
-gis(text, result)
+let { promisify } = require('util')
+let _gis = require('g-i-s')
+let gis = promisify(_gis)
 
-async function result(error, results) {
-if (error) throw error
-C = Math.floor(Math.random() * results.length)
-Random = results[C]
-conn.sendFile(m.chat, Random.url, 'gimage', `*── 「 GOOGLE IMAGE 」 ──*\n\n${text}\n➸ *width*: ${Random.width}\n➸ *height*: ${Random.height}`, m, false, {
-  thumbnail: Buffer.alloc(0)
-})
-}
+let handler  = async (m, { conn, args, text }) => {
+  if (!text) throw 'Cari apa?'
+  let results = await gis(text) || []
+  let { url, width, height } = pickRandom(results) || {}
+  if (!url) throw '404 Not Found'
+  conn.sendFile(m.chat, url, 'gimage', `
+*── 「 GOOGLE IMAGE 」 ──*
+
+${text}
+➸ *width*: ${width}
+➸ *height*: ${height}
+`.trim(), m)
 }
 handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['internet', 'tools']
 handler.command = /^(gimage|image)$/i
 
-handler.limit = true
-
-handler.fail = null
-
 module.exports = handler
+
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length))]
+}
