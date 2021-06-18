331953ec7e937b314a363a9782ec92b602d355e8
Nurutomo <tixbig@gmail.com>
2021-06-18 22:23:32 +0700

    fix: conversation type hidetag

--- plugins/opengumuman.js
+++ plugins/opengumuman.js
@@ -1,3 +1,5 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   let q = m.quoted ? m.quoted : m
@@ -6,29 +8,26 @@ let handler = async (m, { conn, text, participants }) => {
     m.chat,
     conn.prepareMessageFromContent(
       m.chat,
-      { [q.mtype]: c.toJSON ? c.toJSON() : c },
+      { [c.toJSON ? q.mtype : MessageType.extendedText]: c.toJSON ? c.toJSON() : {
+        text: c || ''
+      } },
       {
         contextInfo: {
           mentionedJid: users
-        }
+        },
+        quoted: m
       }
     ),
-    text || q.text
+    text || q.text 
   )
   await conn.relayWAMessage(msg)
 }
-handler.help = ['pengumuman','hidetag'].map(v => 'o' + v + ' [teks]')
-handler.tags = ['owner']
-handler.command = /^(opengumuman|oannounce|ohiddentag|ohidetag)$/i
+handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => 'o' + v + ' [teks]')
+handler.tags = ['group']
+handler.command = /^o(pengumuman|announce|hiddentag|hidetag)$/i
+
 handler.owner = true
-handler.mods = false
-handler.premium = false
 handler.group = true
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 
 module.exports = handler
+
--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -1,3 +1,5 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   let q = m.quoted ? m.quoted : m
@@ -6,7 +8,9 @@ let handler = async (m, { conn, text, participants }) => {
     m.chat,
     conn.prepareMessageFromContent(
       m.chat,
-      { [q.mtype]: c.toJSON ? c.toJSON() : c },
+      { [c.toJSON ? q.mtype : MessageType.extendedText]: c.toJSON ? c.toJSON() : {
+        text: c || ''
+      } },
       {
         contextInfo: {
           mentionedJid: users
@@ -21,17 +25,9 @@ let handler = async (m, { conn, text, participants }) => {
 handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
 handler.tags = ['group']
 handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+
 handler.group = true
-handler.private = false
-
 handler.admin = true
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = true
 
 module.exports = handler
 
