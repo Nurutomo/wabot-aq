40c4aa078818afb9aa1ffa04db9317e6efed3aed
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-06-20 01:04:09 -0700

    thangs ingfo

--- plugins/trigger.js
+++ plugins/trigger.js
@@ -3,7 +3,7 @@
 const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   })
