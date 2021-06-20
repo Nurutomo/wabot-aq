0322dbb20d1e7492a67959917a98364b2ab70528
syahrularranger <74013086+syahrularranger@users.noreply.github.com>
2021-06-20 15:58:49 +0700

    Merge trigger + wasted

--- plugin/trigger-wasted.js
+++ plugin/trigger-wasted.js
@@ -1,13 +1,15 @@
 //kok cewe gw jadi cuek yahh? daritadi pagi gw chat ga dibales cuman di read doang
+//Yo ndak tau kok tanya saya
 
 const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, command }) => {
+  let anu = command.replace('ed', '')
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
+  let hasil = global.API('https://some-random-api.ml', '/canvas/${anu}ed', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   })
-  let stiker = await sticker(null, marah, global.packname, global.author)
+  let stiker = await sticker(null, hasil, global.packname, global.author)
  if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
@@ -15,9 +17,9 @@ let handler = async (m, { conn }) => {
 }
 
 
-handler.help = ['trigger']
+handler.help = ['trigger', 'wasted']
 handler.tags = ['maker']
 
-handler.command = /^(trigger)$/i
+handler.command = /^(wasted|trigger(ed)?)$/i
 
 module.exports = handler
