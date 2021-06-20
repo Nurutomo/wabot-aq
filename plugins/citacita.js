aabe1ce74377dfe27f4e66c136fe3c419c942dc2
Nurutomo <tixbig@gmail.com>
2021-06-20 20:30:57 +0700

    maybe this fix

--- plugins/citacita.js
+++ plugins/citacita.js
@@ -1,23 +1,14 @@
 let fetch = require("node-fetch")
+let arr = []
+fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
+  .then(res => res.text())
+  .then(txt => arr = txt.split('\n'))
 let handler = async (m, { conn }) => {
-let res = await fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
-let txt = await res.text()
-
-let json = txt.split('\n')
-let cita = arr[Math.floor(Math.random() * arr.length)]
-conn.sendFile(m.chat, cita, 'cita.mp3', null, m, true, {
-  type: 'audioMessage', // paksa tanpa convert di ffmpeg
-  ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
-})
+  let cita = arr[Math.floor(Math.random() * arr.length)]
+  if (!cita) throw false
+  await conn.sendFile(m.chat, cita, 'cita.mp3', null, m, true)
 }
-
-
-handler.customPrefix = /^cita cita|citacita|cita$/i
+handler.customPrefix = /^cita( ?cita)$/i
 handler.command = new RegExp
-
-
-
-
-
 
 module.exports = handler
