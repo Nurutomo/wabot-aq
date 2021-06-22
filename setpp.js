51d0891e8f4301411febcfbaaf2f203a66d6532b
Nurutomo <tixbig@gmail.com>
2021-06-22 22:19:04 +0700

    Update setpp.js

--- setpp.js
+++ setpp.js
@@ -1,16 +1,14 @@
 let handler = async (m, { conn, args }) => {
-
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw 'Foto tidak ditemukan!'
-     await conn.updateProfilePicture(m.chat, img)
-    m.reply('Sukses Mengganti Foto Profile Group!')
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || ''
+	if (/image/.test(mime)) {
+		let img = await q.download()
+		if (!img) throw 'Foto tidak ditemukan!'
+	  await conn.updateProfilePicture(m.chat, img)
 	}
-    }
+}
 handler.help = ['setpp']
-handler.command = /^(setpp)$/i
+handler.command = ['setpp']
 handler.group = true
 handler.admin = true
 
