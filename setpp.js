2b800da67599f62216bcbf40430aa9e8e81481ef
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-22 08:03:23 +0700

    Update setpp.js

--- setpp.js
+++ setpp.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/image/.test(mime)) {
-      let img = await conn.downloadM(q)
+      let img = await q.download()
       if (!img) throw 'Foto tidak ditemukan!'
      await conn.updateProfilePicture(m.chat, img)
     m.reply('Sukses Mengganti Foto Profile Group!')
