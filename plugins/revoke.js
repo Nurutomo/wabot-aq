4b8c2d40c27dc218238b1daee8f419732767a105
Nurutomo <tixbig@gmail.com>
2021-06-22 22:12:53 +0700

    Update revoke.js

--- plugins/revoke.js
+++ plugins/revoke.js
@@ -1,22 +1,13 @@
 let handler = async (m, { conn, args, usedPrefix }) => {
-
-let res = await conn.query({
-  json: ["action", "inviteReset", m.chat]
-    })
-  await m.reply('Link Group Berhasil Direset!\n\nLink Baru :\nhttps://chat.whatsapp.com/' + res.code)
+  let res = await conn.revokeInvite(m.chat)
+  m.reply('Link Group Berhasil Direset!\n\nLink Baru:\nhttps://chat.whatsapp.com/' + res.code)
 }
 handler.help = ['revoke']
 handler.tags = ['group']
-handler.command = /^(re)voke$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+handler.command = /^re(voke|new)(invite|link)?$/i
 handler.group = true
-handler.private = false
 
 handler.admin = true
 handler.botAdmin = true
-
-handler.fail = null
 
 module.exports = handler
