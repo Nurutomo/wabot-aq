60cf9df93a6d991aff2811c24acd217fea7419bf
Nurutomo <tixbig@gmail.com>
2021-06-23 14:31:29 +0700

    update anti bug

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -4,7 +4,7 @@ handler.all = async function (m) {
     if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
         m.reply('Bug Troli Detected\n\n' + require('util').format(m.key))
         // await this.clearMessage(m.chat, m.key)
-        await this.modifyChat(m.chat, 'delete', {
+        await this.modifyChat(m.chat, 'clear', {
             includeStarred: false
         }).catch(console.log)
     }
