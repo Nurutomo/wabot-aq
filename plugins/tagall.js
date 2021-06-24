ffde4eba70861aaca2f5b35f82e968e3b173ecb5
Nurutomo <tixbig@gmail.com>
2021-06-24 15:33:45 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,14 +1,14 @@
 let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text, participants }) => {
-let users = participants.map(u => u.jid)
-conn.reply(m.chat, users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, m, {
-contextInfo: { mentionedJid: users }
-})
-
+  let users = participants.map(u => u.jid)
+  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
+  })
 }
-handler.customPrefix = /(@)/
-handler.command = /^everyone|absen$/i
+handler.command = ['tagall', 'absen']
+
+handler.admin = true
 handler.group = true
 
 module.exports = handler
