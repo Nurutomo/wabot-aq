beb44d267442b2755e776ba8a65a00c5d18e4e68
Nurutomo <tixbig@gmail.com>
2021-06-27 17:52:49 +0700

    Update debounce.js

--- plugins/debounce.js
+++ plugins/debounce.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn }) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
   if (global.conn.user.jid == conn.user.jid) {
     await m.reply('Sedang Mereset Bot...\nMohon tunggu sekitar 1 menit')
-    await global.DATABASE.save()
+    await global.db.write()
     process.send('reset')
   } else throw '_eeeeeiiittsssss..._'
 }
