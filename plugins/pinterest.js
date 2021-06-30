9bdfe1619d246ef5e10c47b2d96a33c1218cdcca
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-06-30 19:15:17 +0800

    iya deh maaf

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -5,11 +5,11 @@ let handler = async(m, { conn, text }) => {
   }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let kontol = json[Math.floor(Math.random() * json.length)];
-  conn.sendFile(m.chat, kontol, '', 'selamat menikmati', m)
+  let pint = json[Math.floor(Math.random() * json.length)];
+  conn.sendFile(m.chat, pint, '', 'random photo from keyword', m)
 }
-handler.help = ['pinterest <mengontol>']
+handler.help = ['pinterest <keyword>']
 handler.tags = ['internet']
 handler.command = /^(pinterest)$/i
-//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS dan Ftwrr
 module.exports = handler
