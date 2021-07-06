4c3b3c6331d7c78e1e10ea283f217938da4a972a
Ariffb <arif.febrianto.755@gmail.com>
2021-07-06 16:38:14 +0700

    Update sticker.js
    1. for videos with a maximum duration of 10 seconds
    2. valid URL

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,15 +1,23 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
-let handler  = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (/image|video/.test(mime)) {
+    if (/image/.test(mime)) {
       let img = await q.download()
-      if (!img) throw 'Foto/Video tidak ditemukan'
+      if (!img) throw `balas gambar dengan caption *${usedPrefix + command}*`
       stiker = await sticker(img, false, global.packname, global.author)
-    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
+    } else if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      let img = await q.download()
+      if (!img) throw `balas video/gif dengan caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('URL tidak valid!')
+    }
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
@@ -22,3 +30,7 @@ handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
 module.exports = handler
+
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
