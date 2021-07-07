80c3791596b4c42254ab4cad06fb814b8b5c138a
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-07-07 15:54:12 +0530

    New Command ✅️

--- plugins/short.js
+++ plugins/short.js
@@ -1,7 +1,12 @@
+/* Codded By TOXIC-DEVIL
+# wabot-aq
+# nurutomo/wabot-aq
+LICENSED UNDER GNU General Public */
+
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let url = `https://docs-jojo.herokuapp.com/api/shorturl-at?url=${text}`
-  let res = await fetch(`${url}`)
+  let url = await fetch(global.API('xteam', '/shorturlat', { url: text }, 'APIKEY')) // Powered By Xteam
+  let res = await fetch(`${url}`) 
   let json = await res.json()
   if (json.status) m.reply(`💬 tautan asli : ${text}\n\n🔄 tautan singkat : ${json.result}`)
   else throw json
