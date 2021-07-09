47a152db43c21bda2019be07e0a1af3893038269
kaito12681 <84181607+kaito12681@users.noreply.github.com>
2021-07-09 22:11:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6288231913055', '6288231913055'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -17,7 +17,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'Kaigz'
 
 global.multiplier = 69 // The higher, The harder levelup
 
