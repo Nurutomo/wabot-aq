52b0f032f33c28639fdc5b7991c3be757e296a80
syahrularranger <74013086+syahrularranger@users.noreply.github.com>
2021-07-11 00:46:28 +0700

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -1,13 +1,11 @@
-let { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, text}) => {
-    if (!text) throw 'Siapa yang mau di unbanned?'
+let handler = async (m, { conn }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Tag salah satu yang mau diunban'
     let users = global.db.data.users
     users[who].banned = false
-    conn.reply(m.chat, `berhasil unbanned`, m)
+    conn.reply(m.chat, `Berhasil unbanned`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
