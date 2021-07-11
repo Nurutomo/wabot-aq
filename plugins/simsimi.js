e98f785cfb216f8e6f7451601e95920011c8a7fb
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-07-11 15:21:25 +0530

    Simi Simi Updated ✅️

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) m.reply(json.result)
+  if (json.status) m.reply(`💬 : ${json.result}`)
   else throw json
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
