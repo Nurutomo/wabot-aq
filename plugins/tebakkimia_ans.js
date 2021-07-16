8a4077cecdba640ca113631206b20fad9685d408
syahrularranger <74013086+syahrularranger@users.noreply.github.com>
2021-07-16 17:05:24 +0700

    Update tebakkimia_ans.js

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -2,16 +2,16 @@ let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teki/i.test(m.quoted.text)) return !0
-    conn.tebakkimia = conn.tebakkimia ? conn.tebakkimia : {}
-    if (!(id in conn.tebakkimia)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == conn.tebakkimia[id][0].id) {
-        let json = JSON.parse(JSON.stringify(conn.tebakkimia[id][1]))
+    this.tebakkimia = this.tebakkimia ? this.tebakkimia : {}
+    if (!(id in this.tebakkimia)) return m.reply('Soal itu telah berakhir')
+    if (m.quoted.id == this.tebakkimia[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.tebakkimia[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += conn.tebakkimia[id][2]
-            m.reply(`*Benar!*\n+${conn.tebakkimia[id][2]} XP`)
-            clearTimeout(conn.tebakkimia[id][3])
-            delete conn.tebakkimia[id]
+            global.db.data.users[m.sender].exp += this.tebakkimia[id][2]
+            m.reply(`*Benar!*\n+${this.tebakkimia[id][2]} XP`)
+            clearTimeout(this.tebakkimia[id][3])
+            delete this.tebakkimia[id]
         } else if (m.text.toLowerCase().endsWith(json.name.split` `[1])) m.reply(`*Dikit Lagi!*`)
         else m.reply(`*Salah!*`)
     }
