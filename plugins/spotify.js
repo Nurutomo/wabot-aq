3cdd03059d293b378eb0c17d3307e13bf46fdb0d
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-07-23 05:59:02 -0700

    sebelum pull request di test dulu dong pak

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,11 +1,12 @@
 // Made By github.com/TOXIC-DEVIL
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan nama musik!`
+  if (!text) throw `Masukkan judul musik!`
   let res = await fetch(global.API('zeks', '/api/spotify', { q: text }, 'apikey'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { title, artists, album, thumb, url, preview_mp3 } = json.results[0]
+  if(!json.data[0]) throw json
+  let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
 let spotifyinfo = `✨️ *Title:* ${title}
 🗣️ *Artists:* ${artists}
 🎆️ *Album:* ${album}
@@ -13,8 +14,9 @@ let spotifyinfo = `✨️ *Title:* ${title}
 💚️ *Direct URL:* ${preview_mp3}\n\n@TOXIC-DEVIL`
 
   await conn.sendFile(m.chat, thumb, '', spotifyinfo, m)
+  await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyinfo, m)
 }
-handler.help = ['spotify <tesks>']
+handler.help = ['spotify <query>']
 handler.tags = ['internet']
 handler.command = /^(spotify|music)$/i
 // Made By github.com/TOXIC-DEVIL
