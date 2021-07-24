e96aabe6df70c0a5f65f21a88df70e4b3b90bfc5
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-07-24 04:55:51 -0700

    huff

--- plugins/masak.js
+++ plugins/masak.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.title) throw json
-  await conn.sendFile(m.chat, json.thumb, text, `
+  await conn.sendFile(m.chat, json.thumb, '', `
 ${json.title}
 ${json.url}\n
 ${json.tingkat}
