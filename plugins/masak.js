63329742ff5f49962cf161ef3aa01458f4912b90
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-07-24 04:42:21 -0700

    mengmasak

---
+++ plugins/masak.js
@@ -0,0 +1,22 @@
+let fetch = require("node-fetch")
+
+let handler = async (m, { conn, text }) => {
+  let res = await fetch(global.API('zeks', '/api/resep-masak', { q : text }, 'apikey'))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.title) throw json
+  await conn.sendFile(m.chat, json.thumb, text, `
+${json.title}
+${json.url}\n
+${json.tingkat}
+${json.duration}
+${json.banyak}\n\n
+${json.bahan}
+${json.cara}
+`.trim(), m)
+}
+handler.help = ['resep <makanan>', 'masak <makanan>']
+handler.tags = ['internet']
+handler.command = /^(resep|masak)$/i
+
+module.exports = handler
