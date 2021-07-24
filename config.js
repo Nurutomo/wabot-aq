3295a8da6b3401dd21ef14371a62a85ea2eaac9f
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-07-25 06:45:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.owner = ['62895330379186'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['62895330379186'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
