75a50b3b11889f75c1b50b76192528cec6c4a638
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-07-26 20:21:18 +0530

    Added 1 more info + added emojis for each info ✅️

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -4,10 +4,12 @@ let handler = async(m, { conn, text }) => {
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { name, alternative_names, url, image_url } = json.results[0]
-let charaingfo = `*Name:* ${name}
-*Nickname:* ${alternative_names}
-*Link*: ${url}`
+  let { name, alternative_names, url, image_url, type } = json.results[0]
+let charaingfo = `💬 *Name:* ${name}
+💭 *Nickname:* ${alternative_names}
+🔗 *Link*: ${url}`
+👤 *Character Type*: ${type}
+
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
 }
 handler.help = ['character <nama>']
