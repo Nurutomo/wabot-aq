8d398d3c4a6d591c37a459893caf208049fd495b
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-07-27 10:50:53 +0530

    (!text) msg ✅️

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,10 +1,21 @@
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+  if (teks) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+  else throw 'No Text!'
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
-
 handler.command = /^attp$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
 
 module.exports = handler
