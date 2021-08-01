dd57367712fc011605684c5591e491370485345f
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-01 13:40:46 +0530

    Now, By Restarting the Premium Members will be still Prem. ✅️

--- plugins/delprem.js
+++ plugins/delprem.js
@@ -1,17 +1,18 @@
-let handler = async (m, { conn, text }) => {
+let fs = require('fs')
+let handler = async (m, { text, conn, args }) => {
 
+    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-    else who = m.chat
-    if (!who) throw `tag orangnya!`
-    if (!global.prems.includes(who.split`@`[0])) throw 'dia ngga premium!'
-    let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
-    global.prems.splice(index, 1)
-    conn.reply(m.chat, `@${who.split('@')[0]} sekarang bukan premium!`, m, {
-        contextInfo: {
-            mentionedJid: [who]
-        }
-    })
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    if (json.includes(who)) throw `dia ngga premium!`
+    let index = json.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
+    json.splice(index, 1)
+    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+    m.reply(`@${who.split('@')[0]} sekarang bukan premium!`)
+
+    delete require.cache[require.resolve('../config')]
+    require('../config')
 
 }
 handler.help = ['delprem [@user]']
