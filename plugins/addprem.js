0ad79195243bc2524b02df74c5e6c7eddd8980b0
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-01 13:33:36 +0530

    Now, By Restarting the premium members will be still prem. ✅️

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,22 +1,23 @@
-let handler = async (m, { conn, text }) => {
+let fs = require('fs')
+let handler = async (m, { text, conn, args }) => {
 
+    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-    else who = m.chat
-    if (!who) throw `tag orangnya!`
-    if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
-    global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} sekarang premium!`, m, {
-        contextInfo: {
-            mentionedJid: [who]
-        }
-    })
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    if (json.includes(who.split`@`[0])) throw `dia udah premium!`
+    json.push(`${who.split`@`[0]}`)
+    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+    m.reply(`@${who.split`@`[0]} sekarang premium!`)
+
+    delete require.cache[require.resolve('../config')]
+    require('../config')
 
 }
 handler.help = ['addprem [@user]']
 handler.tags = ['owner']
 handler.command = /^(add|tambah|\+)prem$/i
 
-handler.rowner = true
+handler.owner = true
 
 module.exports = handler
