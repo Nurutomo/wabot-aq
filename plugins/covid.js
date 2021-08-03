ce08aafea7b9fef0c3152642a2d39a97738620fe
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-03 10:32:33 +0530

    Define Error?!

--- plugins/covid.js
+++ plugins/covid.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text, args }) => {
+let handler = async (m, {  text, args }) => {
   let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.confirmed) throw 'Negara?'
   if (json.confirmed) m.reply(`
-Country : ${args[0]}
+Country : args[0]
 Confirmed : ${json.confirmed.value}
 Recovered : ${json.recovered.value}
 Deaths : ${json.deaths.value}
