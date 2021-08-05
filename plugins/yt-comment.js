c289a82312bd8ad9104d4c35b0207fc5dabd1bd7
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-05 16:39:15 +0530

    Added Catch error Image for Get PP ✅️ on ytcomment.js

--- plugins/yt-comment.js
+++ plugins/yt-comment.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'No Text'
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
-    avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
+    avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
     username: conn.getName(m.sender)
   }), 'yt-comment.png', 'Here is your comment', m)
