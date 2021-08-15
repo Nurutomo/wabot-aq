f69424e958c3b15900320fa4333775b003cefac0
Adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-08-15 18:01:42 +0800

    change browser description

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ global.db = new Low(
 global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
+conn.browserDescription = ["wabot-aq", "Desktop", '10.0']
 let authFile = `${opts._[0] || 'session'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
