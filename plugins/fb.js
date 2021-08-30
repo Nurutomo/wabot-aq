ce1d282b8810a5a256c1233a7006e9d609fd42c4
Rizxyu <88314302+Rizxyu@users.noreply.github.com>
2021-08-30 14:57:35 +0700

    Fix Bugs fb.js
    😱👍

--- plugins/fb.js
+++ plugins/fb.js
@@ -2,41 +2,13 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/fb', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) {
-    res.text()
-    throw res.status
-  }
-  let json = await res.json()
-  if (!json.result) throw json
-  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
-  let { name: authorname, url: authorlink } = author || {}
-  let dateConfig = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  }
-  let unknown = '_Unknown_'
-  let none = '_None_'
-  let caption = `
-Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
-Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
-Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
-Size: ${contentSize || unknown}
-Durasi: ${clockString(+ new Date(duration))}
-Genre: ${genre || none}
-Kualitas: ${videoQuality ? videoQuality : unknown}
 
-${description}
+let lol = ''//MASUKKAN APIKEY LU DISINI!
 
-Keyword: ${keywords || none}
-`.trim()
-  conn.sendFile(m.chat, url, 'media-fb', caption, m)
+  let res = await fetch(https://api.lolhuman.xyz/api/facebook?apikey=${lol}&url=${args[0]}`)
+    let json = await res.json()
+  if (!json.result) throw ''
+  conn.sendFile(m.chat, json.result, 'fb.mp4', `Done`, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
@@ -51,3 +23,4 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
+//EROR NANTI DIFIX
