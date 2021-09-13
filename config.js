6f81aa0a7fe700d3d28302e872b107ec401ae52d
assabiq <82302556+Assabiq31213@users.noreply.github.com>
2021-09-13 20:13:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['6285804289247'] // Put your number here
+global.mods = ['6285804289247'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -12,16 +12,16 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.xteam.xyz': '5d975a4873ab86bb',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://api.zeks.xyz': 'Ze3af6X1SjJeUebhHUZCsHMeH13',
   'https://pencarikode.xyz': 'pais',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = '\n         Punya\n\n         SABIQ\n\n\n+6285804289247'
 
 global.multiplier = 69 // The higher, The harder levelup
 
