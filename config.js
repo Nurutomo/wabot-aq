dbd8c1122378c1563640b65d355685168f279806
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2021-09-22 08:56:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6281515589573', '62823380980384'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6281515589573', 62823380980384']// Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'R-Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
