1289573517a7b49d37f42cf0af9723c6986136e5
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-10-01 09:20:43 +0700

    Update delsesittt.js
    Typo dikit

--- plugins/delsesittt.js
+++ plugins/delsesittt.js
@@ -2,10 +2,10 @@ let handler  = async (m, { conn, text }) => {
 	
 conn.game = conn.game ? conn.game : {}
 try {
-if ( conn.game = true ) {
+if ( conn.game === true ) {
 	delete conn.game
 	conn.reply( m.chat, `Berhasil delete session ttt`, m)
-	} else if ( conn.game = false ) {
+	} else if ( conn.game === false ) {
 		m.reply(`Session ttt🎮 tidak ada`)
 		} else throw 'ngabs'
 	} catch (e) {
