c76f8fb85a23ae98cfddca2ec9bce6491f8d2b86
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-10-02 11:40:24 +0700

    Update hug.js

--- plugins/hug.js
+++ plugins/hug.js
@@ -1,4 +1,4 @@
-let Mimetype = require('@adiwajshing/baileys')
+/*let Mimetype = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
 
@@ -16,6 +16,39 @@ let handler = async(m, { conn }) => {
 }
 handler.help = ['neko']
 handler.tags = ['internet']
+handler.command = /^hug$/i
+
+module.exports = handler*/
+
+
+let fetch = require("node-fetch")
+
+const { sticker } = require('../lib/sticker')
+
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn }) => {
+
+  let res = await fetch(global.API('https://api.waifu.pics', '/sfw/hug'))
+
+  let json = await res.json()
+
+  let stiker = await sticker(null, json.url, global.packname, global.author)
+
+  if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+
+    quoted: m
+
+  })
+
+  throw stiker.toString()
+
+}
+
+handler.help = ['neko']
+
+handler.tags = ['internet']
+
 handler.command = /^hug$/i
 
 module.exports = handler
