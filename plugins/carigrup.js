b8ca4b4208a031a41c43f17750fd8b4ae2a42c39
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2021-10-08 16:08:21 +0700

    Update carigrup.js

--- plugins/carigrup.js
+++ plugins/carigrup.js
@@ -1,18 +1,33 @@
 let fetch = require('node-fetch')
+let axios = require ('axios')
+let cheerio = require ('cheerio')
 let handler = async (m, { conn, text }) => {
     if (!text) throw 'Cari apa?'
-    let res = await fetch(global.API('xteam', '/search/grupwa', {
-        q: text
-    }, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
-    let teks = json.result.map(res => res.subject + '\n' + res.link).join('\n\n')
-    m.reply(teks)
+axios.get(`http://ngarang.com/link-grup-wa/daftar-link-grup-wa.php?search=${text}&searchby=name`, {
+method: 'GET'
+}).then(res => {
+let hasal = []
+let hah = cheerio.load(res.data)
+hah('div.wa-chat-body').each(function(c, d) {
+let bokepli = hah(d).find('a').attr('href')
+let wers = hah(d).find('div.wa-chat-title-text').text().trim()
+let result = {
+	name_group: wers,
+	link: bokepli
+}
+hasal.push(result)
+})
+reo = '*Search Group*\n\n'
+for (let i of hasal ) {
+reo += `Name Group: ${i.name_group}\n`
+reo += `Link Group: ${i.link}\n\n`
+}
+m.reply(reo)
+})
 }
 handler.help = ['carigrup <pencarian>']
 handler.tags = ['tools']
-
+handler.limit = true
 handler.command = /^carigrup/i
 
 module.exports = handler
