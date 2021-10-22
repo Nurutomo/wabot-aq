d64ac5ad56d64cd1468c2aa0b392dbcb1c2910cc
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2021-10-22 20:24:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+let fs = require('fs')
+let chalk = require('chalk')
+
 global.owner = ['6281515860089', '6281334177340'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
@@ -20,13 +23,33 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+
+const spack = fs.readFileSync("lib/exif.json")
+const stickerpack = JSON.parse(spack)
+if (stickerpack.spackname == '') {
+  var sticker_name = 'I hope you\'re fine'
+  var sticker_author = 'Nurutomo'
+} else {
+  var sticker_name = stickerpack.spackname
+  var sticker_author = stickerpack.sauthor
+  console.log(sticker_name)
+}
+
+const file_exif = "lib/exif.json"
+fs.watchFile(file_exif, () => {
+  fs.unwatchFile(file_exif)
+  console.log(chalk.redBright("Update 'exif.json'"))
+  delete require.cache[file_exif]
+  require('./lib/exif.json')
+})
+
+global.packname = sticker_name
+global.author = sticker_author
+
 
 global.multiplier = 69 // The higher, The harder levelup
 
-let fs = require('fs')
-let chalk = require('chalk')
+
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
   fs.unwatchFile(file)
