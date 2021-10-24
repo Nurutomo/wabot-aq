836cccd3cd5ca0cc769aefcafcce49ee30e4d3aa
Nurutomo <tixbig@gmail.com>
2021-10-24 07:40:21 +0700

    escape tags

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -5,9 +5,9 @@ let handler = async function (m, { text }) {
   let res = await brainly.search('id', text)
   let answer = res.map(({ question, answers }, i) => `
 _*PERTANYAAN KE ${i + 1}*_
-${question.content}${answers.map((v, i) => `
-*JAWABAN KE ${i + 1}*
-${v.content}`).join``}`).join(`
+${formatTags(question.content)}${answers.map((v, i) => `
+*JAWABAN KE ${i + 1}*${v.verification ? ' (Verified)' : ''}${v.isBest ? ' (Terpilih)' : ''}
+${formatTags(v.content)}`).join``}`).join(`
 
 •------------•
 
@@ -20,3 +20,30 @@ handler.tags = ['internet']
 handler.command = /^brainly$/i
 
 module.exports = handler
+
+function formatTags(str) {
+  let tagRegex = /<(.+?)>((?:.|\n)*?)<\/\1>/gi
+  let replacer = (_, tag, inner) => {
+    let a = inner.replace(tagRegex, replacer)
+    let b = ''
+    switch (tag) {
+      case 'p':
+        a += '\n'
+        break
+      case 'i':
+        b = '_'
+      case 'strikethrough':
+        b = '~'
+      case 'strong':
+        b = '*'
+        a = a.split('\n').map(a => a ? b + a + b : a).join('\n')
+        break
+    }
+    return a
+  }
+  
+  return str
+    .replace(/<br *?\/?>/gi, '\n')
+    .replace(tagRegex, replacer)
+    .trim()
+}
