91958942a3a8090c9f968c0dc5811861bdf3309f
P <81492529+jauhari21@users.noreply.github.com>
2021-10-24 08:22:27 +0700

    renungan by Qara'a
    Gambar yang berisi nasihat

---
+++ plugins/renungan.js
@@ -0,0 +1,16 @@
+let fetch = require("node-fetch")
+let handler = async (m, { conn }) => {
+let res = await fetch('https://raw.githubusercontent.com/jauhari21/asupan/main/100-renungan.txt')
+let txt = await res.text()
+let arr = txt.split('\n')
+let renungan = arr[Math.floor(Math.random() * arr.length)]
+conn.sendFile(m.chat, renungan, 'renungan.jpg', `Sumber : Qara'a`, m) 
+}
+
+handler.help = ['renungan']
+handler.tags = ['quran']
+handler.command = /^renungan$/i
+handler.limit = false
+module.exports = handler
+
+module.exports = handler
