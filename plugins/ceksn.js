48551f74759906f42ab6435fac262eaa340d0d55
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-11-01 18:32:39 +0700

    SN to re-register

---
+++ plugins/ceksn.js
@@ -0,0 +1,15 @@
+const { createHash } = require('crypto')
+
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { conn, text, usedPrefix }) {
+  let sn = createHash('md5').update(m.sender).digest('hex')
+
+m.reply(`
+SN: ${sn}
+`.trim())
+}
+
+handler.help = ['ceksn']
+handler.tags = ['exp']
+handler.command = /^(ceksn)$/i
+module.exports = handler
