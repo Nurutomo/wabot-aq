79a9c3a809d16139b231793a6fd8dab98fb7923a
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-11-03 16:07:24 -0700

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.confirmed) throw 'Negara?'
+let handler = async (m, { args, usedPrefix, command }) => {
+    let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (args[0] || '')))
+    if (!res.ok) throw await res.text()
+        let json = await res.json()
+    if (!args[0]) throw `Contoh :\n${usedPrefix}${command} ID\n\n` + json.countries.map((v) => `${v.name} : ${v.iso2} / ${v.iso3}`).join('\n')
   if (json.confirmed) m.reply(`
-Countries : ${text}
+Countries : ${args[0]}
 Confirmed : ${json.confirmed.value}
 Recovered : ${json.recovered.value}
 Deaths : ${json.deaths.value}
