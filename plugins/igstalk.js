5a5aea72aeabab8ea36e1ba98dd5de2a785b6198
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-11-04 16:56:47 +0700

    No Api

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,41 +1,68 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...username nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/igstalk', {
-    nama: args[0]
-  }, 'APIKEY'))
-  let json = await res.json()
-  if (res.status != 200) throw json
-  if (json.result.error) throw json.result.message
-  let {
-    full_name,
-    username,
-    is_verified,
-    media_count,
-    follower_count,
-    following_count,
-    biography,
-    external_url,
-    profile_pic_url,
-    hd_profile_pic_url_info,
-    is_private
-  } = json.result.user
-  let pp = hd_profile_pic_url_info.url || profile_pic_url
-  let caption = `
-${full_name} *(@${username})* ${is_verified ? '✓' : ''}
-https://instagram.com/${username}
-${is_private ? 'Post Hidden by User' : ('*' + media_count + '* Post(s)')}
-Following *${following_count}* User(s)
-*${follower_count}* Followers
-*Bio:*
-${biography}${external_url ? '\n*External URL:* ' + external_url : ''}
-`.trim()
-  if (pp) conn.sendFile(m.chat, pp, 'ppig.jpg', caption, m)
-  else m.reply(caption)
-}
-handler.help = ['igstalk'].map(v => v + ' <username>')
-handler.tags = ['downloader']
+const cheerio = require('cheerio')
+const axios = require('axios')
+const { JSDOM } = require('jsdom')
+const request = require('request')
+const fetch = require('node-fetch')
+const fs = require('fs')
 
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw 'Uhm...username nya mana?'
+	igstalk(text).then(user => {
+		let igstalkk = JSON.stringify(user)
+        let json = JSON.parse(igstalkk)
+        let hasil = json
+        let iggs = `
+▢ *Username:* ${json.username}
+▢ *Nicname:* ${json.fullName}
+▢ *Followers:* ${json.followers}
+▢ *Following:* ${json.following}
+▢ *Posting:* ${json.postsCount}
+▢ *Link:* https://instagram.com/${json.username}
+▢ *Bio:* ${json.bio}
+`.trim() // tambahin sendiri json.blablabla :)
+         conn.sendFile(m.chat, json.profilePicHD, 'error.jpg', iggs, m, false, { thumbnail: Buffer.alloc(0) })
+      })
+   }
+handler.help = ['igstalk @username']
+handler.tags = ['downloader']
 handler.command = /^(igstalk)$/i
+handler.limit = true
 
 module.exports = handler
+
+
+function igstalk(username){
+	return new Promise((resolve,reject) => {
+axios.get('https://www.instagram.com/'+ username +'/?__a=1',{
+	method: 'GET',
+	headers: {
+"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+"cookie": "mid=XBXl1AALAAEbFoAEfNjZlMMG9dwX; ig_did=91E66A48-5AA2-445D-BFE6-84DC4456DE8F; fbm_124024574287414=base_domain=.instagram.com; ig_nrcb=1; shbid=\"12737\0544008624962\0541656157971:01f72a5102dc07af6845adf923ca70eb86e81ab95fa9dbfdaf157c9eef0e82fd1f10fe23\"; shbts=\"1624621971\0544008624962\0541656157971:01f74841fba8e77a0066b47ea891dec8fba6fdf9216c0816f9fb3532292f769828800ae2\"; fbsr_124024574287414=86D8femzH4_KFW4hd3Z6XFdowU6lG-uXsXRQDNl44VM.eyJ1c2VyX2lkIjoiMTAwMDA0Njc2MDc4Nzg5IiwiY29kZSI6IkFRQngzXzVOejdwVnBwby1LRGRUdEYxUFlzcUdDQXJjcmJfb05HaWFvYkNvOGtLN2paam50bHpvMTNOakFnTzVKOHQ5M0V3U3dvNkRtZ0RiY1l1Z3dQSTIybnExOUxLd3lpZTVfZll0bkNXZXBuM1hoYWFLX0w2R0pZaUpzaDBOTDBhb3pmTVBkRTVQRC12X3FnbUgxLXZYdGVmcHhfaFU0aUZNZVMxNHhFUk5OblJyMmxYTUpDa2RFYTdISXNCR2swdHhaaGF0NUt4UDR3cWZTamRwcVFfQ19sa1RUek5fU0taUTYtMjlzTkdnLUVWb3oxMUZWc3Q2OEx2ZnlIY0V0eFp0ZUxacXpiWmh6MzZrVl83VmFGd0FqVnVkTGFQN2VzT3ZRcmlTQ2pLUE5XbVcyNWhudzIzejJBSnVURW00YWR1cmN6a3ZLWU1icTd2SnN0SVdJV09RIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUJBZmJuQ3haQzZMd3h4MDFJV2MyZ3dsQ3k3Qmp0b05UNUY0WDY2NHBrUzRQeERNVXRsdmhWWkI3SXE0MGsyZ2hJQm55RHRPcW5iVjlPbUNiWGhyTFBaQUhBQjFzVFpBdHF6RFEzVTROUkhOU1V6MFVXWkNtTEdLcDNNWDRoazVIOURLbERHN0QwUlhZNHY4dHBCdVNNYjN4dnBTRGtQcHdYRlBXVU82VCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjI0NjIxOTgxfQ; fbsr_124024574287414=86D8femzH4_KFW4hd3Z6XFdowU6lG-uXsXRQDNl44VM.eyJ1c2VyX2lkIjoiMTAwMDA0Njc2MDc4Nzg5IiwiY29kZSI6IkFRQngzXzVOejdwVnBwby1LRGRUdEYxUFlzcUdDQXJjcmJfb05HaWFvYkNvOGtLN2paam50bHpvMTNOakFnTzVKOHQ5M0V3U3dvNkRtZ0RiY1l1Z3dQSTIybnExOUxLd3lpZTVfZll0bkNXZXBuM1hoYWFLX0w2R0pZaUpzaDBOTDBhb3pmTVBkRTVQRC12X3FnbUgxLXZYdGVmcHhfaFU0aUZNZVMxNHhFUk5OblJyMmxYTUpDa2RFYTdISXNCR2swdHhaaGF0NUt4UDR3cWZTamRwcVFfQ19sa1RUek5fU0taUTYtMjlzTkdnLUVWb3oxMUZWc3Q2OEx2ZnlIY0V0eFp0ZUxacXpiWmh6MzZrVl83VmFGd0FqVnVkTGFQN2VzT3ZRcmlTQ2pLUE5XbVcyNWhudzIzejJBSnVURW00YWR1cmN6a3ZLWU1icTd2SnN0SVdJV09RIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUJBZmJuQ3haQzZMd3h4MDFJV2MyZ3dsQ3k3Qmp0b05UNUY0WDY2NHBrUzRQeERNVXRsdmhWWkI3SXE0MGsyZ2hJQm55RHRPcW5iVjlPbUNiWGhyTFBaQUhBQjFzVFpBdHF6RFEzVTROUkhOU1V6MFVXWkNtTEdLcDNNWDRoazVIOURLbERHN0QwUlhZNHY4dHBCdVNNYjN4dnBTRGtQcHdYRlBXVU82VCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjI0NjIxOTgxfQ; csrftoken=PpiPMEl0R2pAwThsw4NXynO6cVIXHZDo; ds_user_id=38316792800; sessionid=38316792800:rQj5Tr3g5zkg7b:4; rur=\"RVA\05438316792800\0541656158332:01f759cf624bef147397144805bb4c26f6c8b36a232e0f5738c570ee492f6b629f84f6e5\""
+	}
+})
+.then(({ data }) => {
+	const user = data.graphql.user
+let result = {
+id: user.id,
+bio: user.biography,
+followers: user.edge_followed_by.count,
+following: user.edge_follow.count,
+fullName: user.full_name,
+highlightCount: user.highlight_reel_count,
+isBusinessAccount: user.is_business_account,
+isRecentUser: user.is_joined_recently,
+accountCategory: user.business_category_name,
+linkedFacebookPage: user.connected_fb_page,
+isPrivate: user.is_private,
+isVerified: user.is_verified,
+profilePicHD: user.profile_pic_url_hd,
+username: user.username,
+postsCount: user.edge_owner_to_timeline_media.count
+	}
+resolve(result)
+console.log(result)
+})
+	.catch(reject)
+	})
+}
+
