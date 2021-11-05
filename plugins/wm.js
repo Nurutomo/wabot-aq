68f9a9f525242b0ca4a8f2b673351bc8f1ae1d75
Fatur <76021496+Ftwrr@users.noreply.github.com>
2021-11-04 18:37:15 -0700

    fix bug pas wm sticker gif

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,5 +1,6 @@
 const { MessageType } = require("@adiwajshing/baileys");
 const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
+let { webp2mp4 } = require("../lib/webp2mp4");
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [packname, ...author] = text.split`|`;
@@ -8,7 +9,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!m.quoted) throw quot
     if (!/stickerMessage/.test(m.quoted.mtype)) throw quot
     let img = await m.quoted.download();
-    const buffer = await createSticker(img, {
+    let out = img
+    if (m.quoted.isAnimated) {
+      out = await webp2mp4(img)
+    }
+    const buffer = await createSticker(out, {
       type: StickerTypes.CROPPED,
       pack: packname || global.packname,
       author: author || global.author,
