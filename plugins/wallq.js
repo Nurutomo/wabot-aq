4f339c12fd0896f78930dd92476a460f60a0ccad
Fatur <76021496+Ftwrr@users.noreply.github.com>
2021-11-06 05:36:42 -0700

    Update wallq.js

--- plugins/wallq.js
+++ plugins/wallq.js
@@ -1,23 +1,29 @@
-// cewe yang ada di iklan royco bikin ange njing
-// pdhl cuma iklan :v
-
 const fetch = require('node-fetch')
-
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Nyari apa?'
-  let res = await fetch(global.API('https://wall.alphacoders.com/api2.0','/get.php', {
-    auth: '3e7756c85df54b78f934a284c11abe4e',
-    method: 'search',
-    term: text
-  }))
+  let res 
+  if (text) res = await fetch(
+    global.API("https://wall.alphacoders.com/api2.0", "/get.php", {
+      auth: "3e7756c85df54b78f934a284c11abe4e",
+      method: "search",
+      term: text,
+    })
+  )
+    else res = await fetch(
+    global.API("https://wall.alphacoders.com/api2.0", "/get.php", {
+      auth: "3e7756c85df54b78f934a284c11abe4e",
+      method: "latest",
+    })
+  )
+
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
-  await conn.sendFile(m.chat, img.url_image, 'wallpaper', 'Nih wallpaper!', m)
-}
-handler.help = ['wallpaperq <query>']
-handler.tags = ['internet']
-handler.command = /^wall(paper)?q?$/i
-handler.limit = true
+  if (!json.wallpapers) throw json
+  let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)];
+  await conn.sendFile(m.chat, img.url_image, text + `.${img.file_type}`, img.url_page, m);
+};
+handler.help = ["wallpaperq"];
+handler.tags = ["tools"];
+handler.command = /^wall(paper)?q?$/i;
+handler.limit = true;
 
-module.exports = handler
+module.exports = handler;
