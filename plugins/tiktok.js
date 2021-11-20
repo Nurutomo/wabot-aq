b672981eff27ee35508436ee4867231259fc6051
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2021-11-20 23:19:22 +0700

    Create tiktok.js
    Fix xteam api

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args }) => {
   if (res.status !== 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  let url = json.server_1 || json.info[0].videoUrl || ''
+  /*let url = json.server_1 || json.info[0].videoUrl || ''
   if (!url) throw 'Gagal mengambil url download'
   let txt = json.info[0].text
   for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
@@ -20,7 +20,15 @@ let handler = async (m, { conn, args }) => {
 - *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
 - *Desc:*
 ${txt}
-  `.trim(), m)
+  `.trim(), m)*/
+  let url = json.result.link_dl1 || json.result.link_dl2 || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = `
+  - *By:* ${json.result.name}
+  - *Caption:*
+  ${json.result.caption}
+    `
+    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
