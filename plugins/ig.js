3152927240e21286e44dab68c9cf0e5c93f6a25f
Dwi Rizqi <a.dwirizqi.h123@gmail.com>
2021-12-23 12:35:04 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,24 +1,31 @@
 let fetch = require('node-fetch')
+const FormData = require('form-data')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/ig', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) {
-    res.text()
-    throw res.status
+  try {
+    const downloadIg = new FormData();
+    await downloadIg.append('url', args[0])
+    await downloadIg.append('submit', '')
+    await fetch('https://downloadgram.org/#downloadhere', {
+        method: 'post',
+        body: downloadIg,
+        headers: downloadIg.getHeaders()
+    })
+    .then(res => res.text()) 
+    .then(res => {
+        const $ = cheerio.load(res)
+        $(`#downloadBox > a`).each((a, b) => {
+            const urlDownload = $(b).attr('href')
+            const nameFile0 = urlDownload.trim().split('/')[5]
+            const nameFile = nameFile0.split('?')[0]
+            if(urlDownload == undefined || urlDownload == null) return m.reply('Not Found!')
+            conn.sendFile(m.chat, urlDownload, nameFile, '', m)
+        })
+    })
+  } catch (err) {
+    console.log(err)
+    m.reply('Error!')
   }
-  let json = await res.json()
-  if (!json.result) throw json
-  let { name, username, likes, caption, data } = json.result
-  let text = `
-Username: ${name} *(@${username})*
-${likes} Likes
-Caption:
-${caption}
-`.trim()
-  for (let { data: url, type } of data)
-    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), text, m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
