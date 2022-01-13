e41f7000c36443e32d6e14a25b4902850f6b5012
Anurag <55455938+4NUR46@users.noreply.github.com>
2022-01-13 07:44:04 +0530

    Sticker tag changed

--- config.js
+++ config.js
@@ -27,8 +27,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_name = 'Stay'
+  var sticker_author = 'Safe'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
