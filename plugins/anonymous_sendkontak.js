8eb9e46c06d7a1194200f3fab5afebc0addab0bb
Qisyana <79745902+qisyana@users.noreply.github.com>
2022-01-16 20:35:54 +0700

    Update anonymous_sendkontak.js

--- plugins/anonymous_sendkontak.js
+++ plugins/anonymous_sendkontak.js
@@ -6,25 +6,19 @@ wa.me/62896543604477?text=.menu
 Bantu ramein iya 🙏
 */
 
-/*
-Made by Aine
-Anonymous Chat
-wa.me/62896543604477?text=.menu
-Bantu ramein iya 🙏
-*/
-
-let { MessageType, Preslet { MessageType, Presence } = require('@adiwajshing/baileys')
+let { MessageType, Presence } = require('@adiwajshing/baileys')
 
 async function handler(m, { command, conn, text }) {
 	await conn.updatePresence(m.chat, Presence.composing)
 	this.anonymous = this.anonymous ? this.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
+	if (!room) throw 'kamu tidak berada di anonymous chat'
 	let other = room.other(who)
   var name
   if (text) name = text
   else name = conn.getName(m.sender)
-	var number = .split('@')[0]
+	var number = who.split('@')[0]
 	if (other) this.sendMessage(other, `Partner mengirimkan kontak kepadamu`, MessageType.text)
 	if (other) this.sendContact(other, number, name, m)
 }
