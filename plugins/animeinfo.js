11ad56a0409ec4092512051f07eb29bb5ec6229c
ToniOfQ <94815226+ToniOfQ@users.noreply.github.com>
2022-03-01 09:28:45 +0530

    update `animeinfo`

--- plugins/animeinfo.js
+++ plugins/animeinfo.js
@@ -1,32 +1,49 @@
-let fetch = require('node-fetch')
+const { Anime } = require("@shineiichijo/marika");
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} naruto`
-  let res = await fetch(API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  let { title, members, synopsis, episodes, url, rated, score, image_url, type, start_date, end_date, mal_id } = json.results[0]
+  if (!text)
+    throw `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} naruto`;
+  const client = new Anime();
+  let anime;
+  try {
+    anime = await client.searchAnime(text.trim());
+  } catch (error) {
+    throw `An error occurred!`;
+  }
+  const i = anime.data[0];
   //Scrape Genre MAL by DwiR
-  let res2 = await fetch(`https://myanimelist.net/anime/${mal_id}`)
-  if (!res2.ok) throw await res2.text()
-  let html = await res2.text()
-  let { document } = new JSDOM(html).window
-  let genAnim = [...document.querySelectorAll('div[class="spaceit_pad"] > * a')].map(el => el.href).filter(href => href.startsWith('/anime/genre/'))
-  let animeingfo = `✨️ *Title:* ${title}
-🎆️ *Episodes:* ${episodes}
-🎗️ *Genre:* ${genAnim.join(", ")}
-➡️ *Start date:* ${start_date}
-🔚 *End date:* ${end_date}
-💬 *Show Type:* ${type}
-💌️ *Rating:* ${rated}
-❤️ *Score:* ${score}
-👥 *Members:* ${members}
-💚️ *Synopsis:* ${synopsis}
-🌐️ *URL*: ${url}`
-  conn.sendFile(m.chat, image_url, '', animeingfo, m)
-}
-handler.help = ['anime <judul>']
-handler.tags = ['internet']
-handler.command = /^(anime|animeinfo)$/i
-//maapin fatur :<
-module.exports = handler
+  let res2 = await fetch(`https://myanimelist.net/anime/${i.mal_id}`);
+  if (!res2.ok) throw await res2.text();
+  let html = await res2.text();
+  let { document } = new JSDOM(html).window;
+  let genAnim = [...document.querySelectorAll('div[class="spaceit_pad"] > * a')]
+    .map((el) => el.href)
+    .filter((href) => href.startsWith("/anime/genre/"));
+  let Text = `✨️ *Title:* ${i.title}\n🎆️ *Episodes:* ${
+    i.episodes
+  }\n🎗️ *Genres:* ${genAnim.join(", ")}\n➡️ *Start date:* ${
+    i.aired.from
+  }\n🔚 *End date:* ${i.aired.to}\n🍥 *Source:* ${i.source}\n💬 *Show Type:* ${
+    i.type
+  }\n💌️ *Rating:* ${i.rating}\n🤍 *Favorites*: ${i.favorites}\n❤️ *Score:* ${
+    i.score
+  }\n👥 *Members:* ${i.members}\n🏅 *Rank:* ${i.rank}\n🎏 *Popularity Rank:* ${
+    i.popularity
+  }\n`;
+  Text += `📍 *Studios:*`;
+  for (let k = 0; i < i.studios.length; k++) {
+    Text += `\t${i.studios[k].name},`;
+  }
+  Text += `\n🎴 *Producers:*`;
+  for (let k = 0; k < i.producers.length; k++) {
+    Text += `\t${i.producers[k].name},`;
+  }
+  Text += `\n`;
+  if (i.background !== null) Text += `🎋 *Background:* ${i.background}\n`;
+  Text += `💚️ *Synopsis:* ${i.synopsis}\n🌐️ *URL*: ${i.url}`;
+  conn.sendFile(m.chat, i.images.jpg.large_image_url, "", Text, m);
+};
+handler.help = ["anime <judul>"];
+handler.tags = ["internet"];
+handler.command = /^(anime|animeinfo)$/i;
+module.exports = handler;
