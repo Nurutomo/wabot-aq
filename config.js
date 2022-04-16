c16363b7f8e02f20723dc8057c78dd6e68527027
HAD <103582402+hadbotz@users.noreply.github.com>
2022-04-16 12:42:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6285749433476'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'HAD'
 
 global.multiplier = 69 // The higher, The harder levelup
 
