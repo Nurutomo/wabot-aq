064ad1fc0f272bd6c967e0a26e52954dbc9c0d98
Irulbotz <101655172+Zairulkhaq@users.noreply.github.com>
2022-04-20 12:21:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global.owner = ['081334690126', '081334690126', '081334690126'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
-  // name: 'https://website'
+  // name: 'irul ganteng"
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
   zahir: 'https://zahirr-web.herokuapp.com',
