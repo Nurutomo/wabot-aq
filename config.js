5c113201d9a87de10bbce2b1a300348668d059e2
Botsexy <98770207+Botrebelde21@users.noreply.github.com>
2022-04-26 17:05:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global.owner = ['56954940341', '56977774748', '56954940311'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
