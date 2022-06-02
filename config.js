0462c31c6c839c19c4f31ce0de29e27c5002b982
siinopalygy <106498098+siinopalygy@users.noreply.github.com>
2022-06-02 18:33:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global.owner = ['6285714357376'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -28,7 +28,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_author = ' ༄𝑺𝒊 𝑵𝒐𝒑𝒂𝒍 𝒀𝒈𝒚•BoT '
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -43,7 +43,7 @@ fs.watchFile(file_exif, () => {
 })
 
 global.packname = sticker_name
-global.author = sticker_author
+global.author = ༄𝑺𝒊 𝑵𝒐𝒑𝒂𝒍 𝒀𝒈𝒚
 
 
 global.multiplier = 69 // The higher, The harder levelup
