85fb65273208878f8605acc3790e36cf1c0f93c8
Fatih Arridho <71856084+FatihArridho@users.noreply.github.com>
2022-06-16 11:59:38 +0700

    Update brainly.js
    Setelah versi brainly-scraper-v2 menjadi 2.0.13, ada perubahan sedikit.

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -2,7 +2,7 @@ const { Brainly } = require('brainly-scraper-v2')
 const brainly = new Brainly('id')
 let handler = async function (m, { text }) {
   if (!text) throw 'Soalnya?'
-  let res = await brainly.search('id', text)
+  let res = await brainly.search(text)
   let answer = res.map(({ question, answers }, i) => `
 _*PERTANYAAN KE ${i + 1}*_
 ${formatTags(question.content)}${answers.map((v, i) => `
